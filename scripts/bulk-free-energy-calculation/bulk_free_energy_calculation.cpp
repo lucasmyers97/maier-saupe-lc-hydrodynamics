6ae35e418d719bd167e69083090bfaa85caf8395
Lucas Myers <lucasmyers97@gmail.com>
2021-11-01 10:17:57 -0500

    changed bulk-free-energy-calculation script to plot over Cody's Data

--- scripts/bulk-free-energy-calculation/bulk_free_energy_calculation.cpp
+++ scripts/bulk-free-energy-calculation/bulk_free_energy_calculation.cpp
@@ -11,10 +11,14 @@ using vec = dealii::Vector<double>;
 
 int main()
 {
+    std::string filename = "/home/lucas/Documents/grad-work/research/maier-saupe-lc-hydrodynamics/data/bulk-free-energy-calculation/2021-10-26/data.h5";
+    H5Easy::File file(filename, H5Easy::File::Overwrite);
+
     const int vec_dim = 5;
     const int mat_dim = 3;
     const int n_pts = 100;
-    const double S_max = 0.65;
+    const double S_max = 0.7;
+    const double S_min = -0.1;
     const int lebedev_order = 590;
     const double alpha = 1.0;
     const double tol = 1e-12;
@@ -32,66 +36,61 @@ int main()
 
     double log_4_pi = std::log(4*M_PI);
     double log_Z = 0;
-    double Kappa = 3.4049;
+    constexpr int n_kappas = 3;
+    double Kappa[n_kappas] = {3.3, 3.4049, 3.5};
 
     std::vector<double> f(n_pts);
     std::vector<double> S_vec(n_pts);
 
     double S = 0;
     double Z = 0;
-    for (int i = 0; i < n_pts; ++i)
+    for (int j = 0; j < n_kappas; ++j)
     {
-        S = i * (S_max / (n_pts - 1));
-        Q(0, 0) = (2.0/3.0) * S;
-        Q(0, 1) = 0;
-        Q(0, 2) = 0;
-        Q(1, 0) = 0;
-        Q(1, 1) = -(1.0/3.0) * S;
-        Q(1, 2) = 0;
-        Q(2, 0) = 0;
-        Q(2, 1) = 0;
-        Q(2, 2) = -(1.0/3.0) * S;
+        for (int i = 0; i < n_pts; ++i)
+        {
+            S = i * ((S_max - S_min) / (n_pts - 1)) + S_min;
+            Q(0, 0) = (2.0/3.0) * S;
+            Q(0, 1) = 0;
+            Q(0, 2) = 0;
+            Q(1, 0) = 0;
+            Q(1, 1) = -(1.0/3.0) * S;
+            Q(1, 2) = 0;
+            Q(2, 0) = 0;
+            Q(2, 1) = 0;
+            Q(2, 2) = -(1.0/3.0) * S;
 
-        Q_vec(0) = Q(0, 0);
-        Q_vec(3) = Q(1, 1);
-        lm.setQ(Q_vec);
-        lm.returnLambda(Lambda_vec);
-        Z = lm.calcZ();
+            Q_vec(0) = Q(0, 0);
+            Q_vec(3) = Q(1, 1);
+            lm.setQ(Q_vec);
+            lm.returnLambda(Lambda_vec);
+            Z = lm.calcZ();
 
-        Lambda(0, 0) = Lambda_vec(0);
-        Lambda(0, 1) = 0;
-        Lambda(0, 2) = 0;
-        Lambda(1, 0) = 0;
-        Lambda(1, 1) = Lambda_vec(1);
-        Lambda(1, 2) = 0;
-        Lambda(2, 0) = 0;
-        Lambda(2, 1) = 0;
-        Lambda(2, 2) = -(Lambda(0, 0) + Lambda(1, 1));
+            Lambda(0, 0) = Lambda_vec(0);
+            Lambda(0, 1) = 0;
+            Lambda(0, 2) = 0;
+            Lambda(1, 0) = 0;
+            Lambda(1, 1) = Lambda_vec(1);
+            Lambda(1, 2) = 0;
+            Lambda(2, 0) = 0;
+            Lambda(2, 1) = 0;
+            Lambda(2, 2) = -(Lambda(0, 0) + Lambda(1, 1));
 
-        Q.mmult(Q_squared, Q);
-        // Q_squared.print_formatted(std::cout);
-        trace_Q_squared = Q_squared.trace();
+            Q.mmult(Q_squared, Q);
+            // Q_squared.print_formatted(std::cout);
+            trace_Q_squared = Q_squared.trace();
 
-        Lambda.mmult(Lambda_Q, Q);
-        trace_Lambda_Q = Lambda_Q.trace();
+            Lambda.mmult(Lambda_Q, Q);
+            trace_Lambda_Q = Lambda_Q.trace();
 
-        log_Z = std::log(Z);
+            log_Z = std::log(Z);
 
-        f[i] = -Kappa*trace_Q_squared + log_4_pi - log_Z + trace_Lambda_Q;
-        S_vec[i] = S;
+            f[i] = -Kappa[j]*trace_Q_squared + log_4_pi - log_Z + trace_Lambda_Q;
+            S_vec[i] = S;
+        }
 
-        // if (i == 0)
-        // {
-        //     std::cout << trace_Q_squared << "\n";
-        //     std::cout << log_Z << "\n";
-        //     std::cout << log_4_pi << "\n";
-        //     std::cout << trace_Lambda_Q << "\n";
-        // }
+        H5Easy::dump(file, "/free_energy_" + std::to_string(Kappa[j]), f);
     }
-
-    std::string filename = "/home/lucasmyers97/maier-saupe-lc-hydrodynamics/data/bulk-free-energy-calculation/2021-10-08/data.h5";
-    H5Easy::File file(filename, H5Easy::File::Overwrite);
-    H5Easy::dump(file, "/free_energy", f);
+    
     H5Easy::dump(file, "/S_val", S_vec);
 
     return 0;
