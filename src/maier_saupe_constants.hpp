7f486e7dd555b99cb8f73fcfe11d4885e0e13daf
Lucas Myers <lucasmyers97@gmail.com>
2021-11-09 15:15:04 -0600

    added templated constants for liquid crystal systems

---
+++ src/maier_saupe_constants.hpp
@@ -0,0 +1,53 @@
+#ifndef MAIER_SAUPE_CONSTANTS_HPP
+#define MAIER_SAUPE_CONSTANTS_HPP
+
+#include <array> 
+
+namespace maier_saupe_constants{
+
+    // dim of (i) vector representing Q-tensor, and (ii) matrix of Q-tensor
+    template <int space_dim>
+    static constexpr int mat_dim{3};
+    template <int space_dim>
+    static constexpr int vec_dim{};
+
+    // Q-tensor degrees of freedom for 3- and 2-dimensional systems
+    template<>
+    static constexpr int vec_dim<3> = 5;
+    template<>
+    static constexpr int vec_dim<2> = 3;
+
+    // alias Q-tensor vector and matrix classes
+    template <int space_dim>
+    using vec = std::array<int, vec_dim<space_dim>>;
+    template <int space_dim>
+    using mat = std::array< std::array<int, mat_dim<space_dim>>, 
+                            mat_dim<space_dim> >;
+
+    // (i, j) indices in Q-tensor given Q-vector index
+    template <int space_dim>
+    static constexpr vec<space_dim> Q_row = {{}};
+    template <int space_dim>
+    static constexpr vec<space_dim> Q_col = {{}};
+    
+    // explicit indices for 3- and 2-spatial dimensions
+    template <>
+    static constexpr vec<3> Q_row<3> = { {0, 0, 0, 1, 1} };
+    template <>
+    static constexpr vec<3> Q_col<3> = { {0, 1, 2, 1, 2} };
+    template <>
+    static constexpr vec<2> Q_row<2> = { {0, 0, 1} };
+    template <>
+    static constexpr vec<2> Q_col<2> = { {0, 1, 1} };
+
+    // Q-vector indices given Q-tensor (i, j) indices (except (3, 3) entry)
+    template <int space_dim>
+    static constexpr mat<space_dim> Q_idx = {{{0, 1, 2}, {1, 3, 4}, {2, 4, 0}}};
+
+    // Kronecker delta
+    template <int space_dim>
+    static constexpr mat<space_dim> delta = {{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}};
+
+}
+
+#endif
