8de23bad0be7b59abe582774ff49da0bc00e515b
Lucas Myers <lucasmyers97@gmail.com>
2021-11-22 10:45:57 -0600

    added better command-line arguments

--- app/analysis/comparing-to-cody-data.py
+++ app/analysis/comparing-to-cody-data.py
@@ -1,30 +1,72 @@
+import sys
+import argparse
+import re
 import scipy.io as sio
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-def readCodyData():
-
-    # get file location from environment variable + relative location
-    maier_saupe_dir = os.getenv('MAIER_SAUPE_DIR')
-    cody_mat_filename = os.path.join(
-                                maier_saupe_dir, 
-                                'examples', 
-                                'cody_data', 
-                                'PlusHalfQ.mat')
+def readCodyData(cody_data_filename):
 
     # read in Cody's data
-    cody_data = sio.loadmat(cody_mat_filename)
+    cody_data = h5py.File(cody_data_filename, "r")
     
     # parse data
-    Q = cody_data['Q']
-    X = cody_data['X']
-    Y = cody_data['Y']
+    if 'Q1' in cody_data.keys():
+        Q1 = cody_data['Q1'][:]
+        Q2 = cody_data['Q2'][:]
+        Q3 = cody_data['Q3'][:]
+        Q4 = cody_data['Q4'][:]
+        Q5 = cody_data['Q5'][:]
+        Q = parseQVector(Q1, Q2, Q3, Q4, Q5)
+
+        X = cody_data['X']
+        Y = cody_data['Y']
+
+    elif 'eta' in cody_data.keys():
+        eta = cody_data['eta'][:]
+        mu = cody_data['mu'][:]
+        nu = cody_data['nu'][:]
+        Q = parseAuxVariables(eta, mu, nu, True)
+
+        x = cody_data['x'][:]
+        X, Y = np.meshgrid(x, x, indexing='ij')
 
     return Q, X, Y
 
+def parseQVector(Q1, Q2, Q3, Q4, Q5):
 
+    Q = np.zeros((3, 3) + np.shape(Q1))
+    Q[0, 0, :, :] = Q1
+    Q[0, 1, :, :] = Q2
+    Q[0, 2, :, :] = Q3
+    Q[1, 0, :, :] = Q2
+    Q[1, 1, :, :] = Q4
+    Q[1, 2, :, :] = Q5
+    Q[2, 0, :, :] = Q3
+    Q[2, 1, :, :] = Q5
+    Q[2, 2, :, :] = -(Q1 + Q4)
+    
+    return Q
+
+def parseAuxVariables(eta, mu, nu, old_convention=False):
+
+    if old_convention:
+        eta *= 1 / np.sqrt(3)
+
+    Q = np.zeros((3, 3) + np.shape(eta))
+    Q[0, 0, :, :] = 2 / np.sqrt(3) * eta
+    Q[0, 1, :, :] = nu
+    Q[0, 2, :, :] = 0
+    Q[1, 0, :, :] = nu
+    Q[1, 1, :, :] = -1 / np.sqrt(3) * eta + mu
+    Q[1, 2, :, :] = 0
+    Q[2, 0, :, :] = 0
+    Q[2, 1, :, :] = 0
+    Q[2, 2, :, :] = -1 / np.sqrt(3) * eta - mu
+        
+    return Q
 
 def calcDirectorAndS(Q):
 
@@ -78,13 +120,7 @@ def plotDirectorAndS(X, Y, n, S, title="Defect configuration"):
 
 
 
-def readLucasData():
-
-    # get file location from environment variable + relative location
-    maier_saupe_dir = os.getenv('MAIER_SAUPE_DIR')
-    lucas_data_filename = os.path.join(
-                                maier_saupe_dir, 'data', 'IsoSteadyState', 
-                                '2021-09-16', 'minus-half-defect-me.hdf5')
+def readLucasData(lucas_data_filename):
     
     with h5py.File(lucas_data_filename, "r") as f:
         Q1_dset = f['Q1']
@@ -187,30 +223,74 @@ def plotQNormedDifference(X, Y, Q_diff, title="Normed Q difference"):
 
 if __name__ == "__main__":
 
+    description = "Plots cody's data, my data, and then the norm of the difference"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--cody_folder', dest='cody_folder',
+                        help='folder where data from cody is stored')
+    parser.add_argument('--cody_filename', dest='cody_filename',
+                        help='name of data file from cody')
+    parser.add_argument('--lucas_folder', dest='lucas_folder',
+                        help='folder where data from lucas is stored')
+    parser.add_argument('--lucas_filename', dest='lucas_filename',
+                        help='name of data file from lucas')
+    parser.add_argument('--cody_plot_filename', dest='cody_plot_filename',
+                        help='filename of plot of cody data')
+    parser.add_argument('--lucas_plot_filename', dest='lucas_plot_filename',
+                        help='filename of plot of lucas data')
+    parser.add_argument('--diff_plot_filename', dest='diff_plot_filename',
+                        help='filename of normed difference plot')
+    args = parser.parse_args() 
+
+    cody_data_filename = os.path.join(args.cody_folder, args.cody_filename)
+    if args.cody_plot_filename:
+        cody_plot_filename = args.cody_plot_filename
+    else:
+        cody_plot_filename = re.sub(r'(.*).h5', r'\1.png', cody_data_filename)
+
     # read and plot cody data
-    Q_cody, X, Y = readCodyData()
+    Q_cody, X, Y = readCodyData(cody_data_filename)
     n_cody, S_cody = calcDirectorAndS(Q_cody)
     fig_cody, _, _ = plotDirectorAndS(
                             X, Y, n_cody, S_cody,
                             title="-1/2 Defect from Cody, Isotropic elasticity")
     plot_filename_cody = "min_1-2_defect_iso_cody.png"
-    fig_cody.savefig(plot_filename_cody)
+    fig_cody.savefig(cody_plot_filename)
 
-    # read and plot lucas data
-    Q_lucas, X, Y = readLucasData()
-    Q_lucas = rotateQ(Q_lucas)
-    n_lucas, S_lucas = calcDirectorAndS(Q_lucas)
-    fig_lucas, _, _ = plotDirectorAndS(
-                            X, Y, n_lucas, S_lucas,
-                            title="-1/2 Defect from Lucas, Isotropic elasticity"
-                            )
-    plot_filename_lucas = "min_1-2_defect_iso_lucas.png"
-    fig_lucas.savefig(plot_filename_lucas)
+    if args.lucas_filename:
+        if args.lucas_folder:
+            lucas_folder = args.lucas_folder
+        else:
+            lucas_folder = args.cody_folder
+            
+        lucas_data_filename = os.path.join(lucas_folder, args.lucas_filename)
 
-    # plot normed difference
-    Q_diff = calcQNormedDifference(Q_cody, Q_lucas)
-    fig_diff, _ = plotQNormedDifference(
-                        X, Y, Q_diff, 
-                        title="Normed difference between Cody and Lucas data")
-    plot_filename_diff = "min_1-2-defect_iso_difference.png"
-    fig_diff.savefig(plot_filename_diff)
+        if args.lucas_plot_filename:
+            lucas_plot_filename = os.path.join(lucas_folder, 
+                                               args.lucas_plot_filename)
+        else:
+            lucas_plot_filename = re.sub(r'(.*).h5', r'\1.png', 
+                                         lucas_data_filename)
+                                         
+        # read and plot lucas data
+        Q_lucas, X, Y = readLucasData()
+        Q_lucas = rotateQ(Q_lucas)
+        n_lucas, S_lucas = calcDirectorAndS(Q_lucas)
+        fig_lucas, _, _ = plotDirectorAndS(
+                                X, Y, n_lucas, S_lucas,
+                                title="-1/2 Defect from Lucas, Isotropic elasticity"
+                                )
+        plot_filename_lucas = "min_1-2_defect_iso_lucas.png"
+        fig_lucas.savefig(plot_filename_lucas)
+
+        if args.diff_plot_filename:
+            plot_filename_diff = os.path.join(lucas_folder, diff_plot_filename)
+        else:
+            plot_filename_diff = os.path.join(lucas_folder, "diff-plot.png")
+
+        # plot normed difference
+        Q_diff = calcQNormedDifference(Q_cody, Q_lucas)
+        fig_diff, _ = plotQNormedDifference(
+                            X, Y, Q_diff, 
+                            title="Normed difference between Cody and Lucas data")
+        plot_filename_diff = "min_1-2-defect_iso_difference.png"
+        fig_diff.savefig(plot_filename_diff)
