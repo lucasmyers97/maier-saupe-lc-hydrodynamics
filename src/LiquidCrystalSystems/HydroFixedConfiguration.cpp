c27dd5a661903a445187f46fbfa9c863929a98e5
Lucas Myers <lucasmyers97@gmail.com>
2022-03-13 22:33:03 -0500

    Made solution vector a block vector to hold Q-vector
    In order to use a Q-vector field as a source, I need to structure it as
    something which is handled by the same DoFHandler object. This way we can index
    the quadrature values of the Q-tensor using the same cell iterator. The only way
    I could think to do this is to structure it as a BlockVector object which only
    couples to itself (and not the velocity or pressure). This works, but it was a
    huge pain to implement because of difficult-to-understand errors (e.g. seg
    faults, bad linked lists, etc.). This commit gets it to work, however.

--- src/LiquidCrystalSystems/HydroFixedConfiguration.cpp
+++ src/LiquidCrystalSystems/HydroFixedConfiguration.cpp
@@ -36,6 +36,10 @@
 #include <fstream>
 #include <memory>
 
+#include <Utilities/maier_saupe_constants.hpp>
+
+namespace msc = maier_saupe_constants;
+
 template <int dim>
 struct InnerPreconditioner;
 
@@ -90,7 +94,7 @@ class BoundaryValues : public dealii::Function<dim>
 {
 public:
     BoundaryValues()
-        : dealii::Function<dim>(dim + 1)
+        : dealii::Function<dim>(dim + 1 + msc::vec_dim<dim>)
     {}
 
     virtual double value(const dealii::Point<dim> & p,
@@ -128,7 +132,7 @@ class RightHandSide : public dealii::Function<dim>
 {
 public:
     RightHandSide()
-        : dealii::Function<dim>(dim + 1)
+        : dealii::Function<dim>(dim + 1 + msc::vec_dim<dim>)
     {}
 
     virtual double value(const dealii::Point<dim> & p,
@@ -244,7 +248,9 @@ template <int dim>
 HydroFixedConfiguration<dim>::HydroFixedConfiguration(const unsigned int degree)
     : degree(degree)
     , triangulation(dealii::Triangulation<dim>::maximum_smoothing)
-    , fe(dealii::FE_Q<dim>(degree + 1), dim, dealii::FE_Q<dim>(degree), 1)
+    , fe(dealii::FE_Q<dim>(degree + 1), dim,
+         dealii::FE_Q<dim>(degree), 1,
+         dealii::FE_Q<dim>(degree + 1), msc::vec_dim<dim>)
     , dof_handler(triangulation)
 {}
 
@@ -259,8 +265,10 @@ void HydroFixedConfiguration<dim>::setup_dofs()
     dof_handler.distribute_dofs(fe);
     dealii::DoFRenumbering::Cuthill_McKee(dof_handler);
 
-    std::vector<unsigned int> block_component(dim + 1, 0);
+    std::vector<unsigned int> block_component(dim + 1 + msc::vec_dim<dim>, 0);
     block_component[dim] = 1;
+    for (int i = dim + 1; i < block_component.size(); ++i)
+        block_component[i] = 2;
     dealii::DoFRenumbering::component_wise(dof_handler, block_component);
 
     {
@@ -271,7 +279,7 @@ void HydroFixedConfiguration<dim>::setup_dofs()
         dealii::VectorTools::interpolate_boundary_values(dof_handler,
                                                          0,
                                                          // BoundaryValues<dim>(),
-                                                         dealii::Functions::ZeroFunction<dim>(dim + 1),
+                                                         dealii::Functions::ZeroFunction<dim>(dim + 1 + msc::vec_dim<dim>),
                                                          constraints,
                                                          fe.component_mask(velocities));
     }
@@ -282,30 +290,42 @@ void HydroFixedConfiguration<dim>::setup_dofs()
         dealii::DoFTools::count_dofs_per_fe_block(dof_handler, block_component);
     const unsigned int n_u = dofs_per_block[0];
     const unsigned int n_p = dofs_per_block[1];
+    const unsigned int n_Q = dofs_per_block[2];
 
     std::cout << "   Number of active cells: " << triangulation.n_active_cells()
               << std::endl
               << "   Number of degrees of freedom: " << dof_handler.n_dofs()
-              << " (" << n_u << '+' << n_p << ')' << std::endl;
+              << " (" << n_u << '+' << n_p << '+' << n_Q << ')' << std::endl;
 
     {
-        dealii::BlockDynamicSparsityPattern dsp(2, 2);
+        dealii::BlockDynamicSparsityPattern dsp(3, 3);
 
         dsp.block(0, 0).reinit(n_u, n_u);
         dsp.block(1, 0).reinit(n_p, n_u);
+        dsp.block(2, 0).reinit(n_Q, n_u);
         dsp.block(0, 1).reinit(n_u, n_p);
         dsp.block(1, 1).reinit(n_p, n_p);
+        dsp.block(2, 1).reinit(n_Q, n_p);
+        dsp.block(0, 2).reinit(n_u, n_Q);
+        dsp.block(1, 2).reinit(n_p, n_Q);
+        dsp.block(2, 2).reinit(n_Q, n_Q);
 
         dsp.collect_sizes();
 
-        dealii::Table<2, dealii::DoFTools::Coupling> coupling(dim + 1, dim + 1);
+        dealii::Table<2, dealii::DoFTools::Coupling>
+            coupling(dim + 1 + msc::vec_dim<dim>, dim + 1 + msc::vec_dim<dim>);
 
-        for (unsigned int c = 0; c < dim + 1; ++c)
-            for (unsigned int d = 0; d < dim + 1; ++d)
-                if (!((c == dim) && (d == dim)))
+        for (unsigned int c = 0; c < dim + 1 + msc::vec_dim<dim>; ++c)
+            for (unsigned int d = 0; d < dim + 1 + msc::vec_dim<dim>; ++d)
+                if ((c < dim) && (d < dim))
+                    coupling[c][d] = dealii::DoFTools::always;
+                else if ((c < dim) && (d == dim))
+                    coupling[c][d] = dealii::DoFTools::always;
+                else if ((c == dim) && (d < dim))
                     coupling[c][d] = dealii::DoFTools::always;
                 else
                     coupling[c][d] = dealii::DoFTools::none;
+
 
         dealii::DoFTools::make_sparsity_pattern(dof_handler,
                                                 coupling,
@@ -317,23 +337,30 @@ void HydroFixedConfiguration<dim>::setup_dofs()
     }
 
     {
-        dealii::BlockDynamicSparsityPattern preconditioner_dsp(2, 2);
+        dealii::BlockDynamicSparsityPattern preconditioner_dsp(3, 3);
 
         preconditioner_dsp.block(0, 0).reinit(n_u, n_u);
         preconditioner_dsp.block(1, 0).reinit(n_p, n_u);
+        preconditioner_dsp.block(2, 0).reinit(n_Q, n_u);
         preconditioner_dsp.block(0, 1).reinit(n_u, n_p);
         preconditioner_dsp.block(1, 1).reinit(n_p, n_p);
+        preconditioner_dsp.block(2, 1).reinit(n_Q, n_p);
+        preconditioner_dsp.block(0, 2).reinit(n_u, n_Q);
+        preconditioner_dsp.block(1, 2).reinit(n_p, n_Q);
+        preconditioner_dsp.block(2, 2).reinit(n_Q, n_Q);
 
         preconditioner_dsp.collect_sizes();
 
-        dealii::Table<2, dealii::DoFTools::Coupling> preconditioner_coupling(dim + 1, dim + 1);
+        dealii::Table<2, dealii::DoFTools::Coupling>
+            preconditioner_coupling(dim + 1 + msc::vec_dim<dim>,
+                                    dim + 1 + msc::vec_dim<dim>);
 
-        for (unsigned int c = 0; c < dim + 1; ++c)
-            for (unsigned int d = 0; d < dim + 1; ++d)
-                if (((c == dim) && (d == dim)))
-                    preconditioner_coupling[c][d] = dealii::DoFTools::always;
-                else
-                    preconditioner_coupling[c][d] = dealii::DoFTools::none;
+        for (unsigned int c = 0; c < dim + 1 + msc::vec_dim<dim>; ++c)
+          for (unsigned int d = 0; d < dim + 1 + msc::vec_dim<dim>; ++d)
+            if ((c == dim) && (d == dim))
+              preconditioner_coupling[c][d] = dealii::DoFTools::always;
+            else
+              preconditioner_coupling[c][d] = dealii::DoFTools::none;
 
         dealii::DoFTools::make_sparsity_pattern(dof_handler,
                                                 preconditioner_coupling,
@@ -347,14 +374,16 @@ void HydroFixedConfiguration<dim>::setup_dofs()
     system_matrix.reinit(sparsity_pattern);
     preconditioner_matrix.reinit(preconditioner_sparsity_pattern);
 
-    solution.reinit(2);
+    solution.reinit(3);
     solution.block(0).reinit(n_u);
     solution.block(1).reinit(n_p);
+    solution.block(2).reinit(n_Q);
     solution.collect_sizes();
 
-    system_rhs.reinit(2);
+    system_rhs.reinit(3);
     system_rhs.block(0).reinit(n_u);
     system_rhs.block(1).reinit(n_p);
+    system_rhs.block(2).reinit(n_Q);
     system_rhs.collect_sizes();
 }
 
@@ -387,8 +416,9 @@ void HydroFixedConfiguration<dim>::assemble_system()
     std::vector<dealii::types::global_dof_index> local_dof_indices(dofs_per_cell);
 
     const RightHandSide<dim>    right_hand_side;
-    std::vector<dealii::Vector<double>> rhs_values(n_q_points,
-                                                   dealii::Vector<double>(dim + 1));
+    std::vector<dealii::Vector<double>>
+        rhs_values(n_q_points,
+                   dealii::Vector<double>(dim + 1 + msc::vec_dim<dim>));
 
     const dealii::FEValuesExtractors::Vector velocities(0);
     const dealii::FEValuesExtractors::Scalar pressure(dim);
@@ -397,74 +427,104 @@ void HydroFixedConfiguration<dim>::assemble_system()
     std::vector<double>                  div_phi_u(dofs_per_cell);
     std::vector<double>                  phi_p(dofs_per_cell);
 
+    bool printed = false;
+
     for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+        fe_values.reinit(cell);
+        local_matrix                = 0;
+        local_preconditioner_matrix = 0;
+        local_rhs                   = 0;
+
+        right_hand_side.vector_value_list(fe_values.get_quadrature_points(),
+                                          rhs_values);
+
+        for (unsigned int q = 0; q < n_q_points; ++q)
         {
-            fe_values.reinit(cell);
-            local_matrix                = 0;
-            local_preconditioner_matrix = 0;
-            local_rhs                   = 0;
-
-            right_hand_side.vector_value_list(fe_values.get_quadrature_points(),
-                                              rhs_values);
-
-            for (unsigned int q = 0; q < n_q_points; ++q)
-                {
-                    for (unsigned int k = 0; k < dofs_per_cell; ++k)
-                        {
-                            symgrad_phi_u[k] =
-                                fe_values[velocities].symmetric_gradient(k, q);
-                            div_phi_u[k] = fe_values[velocities].divergence(k, q);
-                            phi_p[k]     = fe_values[pressure].value(k, q);
-                        }
-
-                    for (unsigned int i = 0; i < dofs_per_cell; ++i)
-                        {
-                            for (unsigned int j = 0; j <= i; ++j)
-                                {
-                                    local_matrix(i, j) +=
-                                        (2 * (symgrad_phi_u[i] * symgrad_phi_u[j]) // (1)
-                                         - div_phi_u[i] * phi_p[j]                 // (2)
-                                         - phi_p[i] * div_phi_u[j])                // (3)
-                                        * fe_values.JxW(q);                        // * dx
-
-                                    local_preconditioner_matrix(i, j) +=
-                                        (phi_p[i] * phi_p[j]) // (4)
-                                        * fe_values.JxW(q);   // * dx
-                                }
-                            const unsigned int component_i =
-                                fe.system_to_component_index(i).first;
-                            local_rhs(i) += (fe_values.shape_value(i, q)   // (phi_u_i(x_q)
-                                             * rhs_values[q](component_i)) // * f(x_q))
-                                * fe_values.JxW(q);            // * dx
-                        }
-                }
+            for (unsigned int k = 0; k < dofs_per_cell; ++k)
+            {
+                const unsigned int component_k =
+                    fe.system_to_component_index(k).first;
+                if (component_k > dim)
+                    continue;
+                symgrad_phi_u[k] =
+                    fe_values[velocities].symmetric_gradient(k, q);
+                div_phi_u[k] = fe_values[velocities].divergence(k, q);
+                phi_p[k]     = fe_values[pressure].value(k, q);
+            }
 
             for (unsigned int i = 0; i < dofs_per_cell; ++i)
-                for (unsigned int j = i + 1; j < dofs_per_cell; ++j)
-                    {
-                        local_matrix(i, j) = local_matrix(j, i);
-                        local_preconditioner_matrix(i, j) =
-                            local_preconditioner_matrix(j, i);
-                    }
+            {
+              const unsigned int component_i =
+                  fe.system_to_component_index(i).first;
+              if (component_i > dim)
+                  continue;
 
-            cell->get_dof_indices(local_dof_indices);
-            constraints.distribute_local_to_global(local_matrix,
-                                                   local_rhs,
-                                                   local_dof_indices,
-                                                   system_matrix,
-                                                   system_rhs);
-            constraints.distribute_local_to_global(local_preconditioner_matrix,
-                                                   local_dof_indices,
-                                                   preconditioner_matrix);
+              for (unsigned int j = 0; j <= i; ++j)
+              {
+                  const unsigned int component_j =
+                      fe.system_to_component_index(j).first;
+                  if (component_j > dim)
+                      continue;
+
+                  local_matrix(i, j) +=
+                      (2 * (symgrad_phi_u[i] * symgrad_phi_u[j]) // (1)
+                       - div_phi_u[i] * phi_p[j]                 // (2)
+                       - phi_p[i] * div_phi_u[j])                // (3)
+                      * fe_values.JxW(q);                        // * dx
+
+                  local_preconditioner_matrix(i, j) +=
+                      (phi_p[i] * phi_p[j]) // (4)
+                      * fe_values.JxW(q);   // * dx
+              }
+                local_rhs(i) += (fe_values.shape_value(i, q)   // (phi_u_i(x_q)
+                                 * rhs_values[q](component_i)) // * f(x_q))
+                    * fe_values.JxW(q);            // * dx
+            }
         }
+
+        for (unsigned int i = 0; i < dofs_per_cell; ++i)
+        {
+            const unsigned int component_i =
+                fe.system_to_component_index(i).first;
+            if (component_i > dim)
+                continue;
+            for (unsigned int j = i + 1; j < dofs_per_cell; ++j)
+            {
+                const unsigned int component_j =
+                    fe.system_to_component_index(j).first;
+                if (component_j > dim)
+                    continue;
+                local_matrix(i, j) = local_matrix(j, i);
+                local_preconditioner_matrix(i, j) =
+                    local_preconditioner_matrix(j, i);
+            }
+        }
+
+        cell->get_dof_indices(local_dof_indices);
+        constraints.distribute_local_to_global(local_matrix,
+                                               local_rhs,
+                                               local_dof_indices,
+                                               system_matrix,
+                                               system_rhs);
+        // constraints.distribute_local_to_global(local_matrix,
+        //                                        local_dof_indices,
+        //                                        preconditioner_matrix);
+        constraints.distribute_local_to_global(local_preconditioner_matrix,
+                                               local_dof_indices,
+                                               preconditioner_matrix);
+    }
 
     std::cout << "   Computing preconditioner..." << std::endl << std::flush;
 
     A_preconditioner =
         std::make_shared<typename InnerPreconditioner<dim>::type>();
-    A_preconditioner->initialize(
-                                 system_matrix.block(0, 0),
+    std::cout << system_matrix.block(0, 0).m() << " by "
+              << system_matrix.block(0, 0).n() << std::endl;
+    A_preconditioner->factorize(system_matrix.block(0, 0));
+    A_preconditioner->initialize(system_matrix.block(0, 0),
                                  typename InnerPreconditioner<dim>::type::AdditionalData());
+    std::cout << "done computing preconditioner..." << std::endl;
 }
 
 
@@ -522,11 +582,19 @@ HydroFixedConfiguration<dim>::output_results(const unsigned int refinement_cycle
 {
     std::vector<std::string> solution_names(dim, "velocity");
     solution_names.emplace_back("pressure");
+    solution_names.emplace_back("Q1");
+    solution_names.emplace_back("Q2");
+    solution_names.emplace_back("Q3");
+    solution_names.emplace_back("Q4");
+    solution_names.emplace_back("Q5");
 
     std::vector<dealii::DataComponentInterpretation::DataComponentInterpretation>
         data_component_interpretation(dim,
                                       dealii::DataComponentInterpretation::component_is_part_of_vector);
     data_component_interpretation.push_back(dealii::DataComponentInterpretation::component_is_scalar);
+    for (int i = 0; i < msc::vec_dim<dim>; ++i)
+      data_component_interpretation.push_back(
+          dealii::DataComponentInterpretation::component_is_scalar);
 
     dealii::DataOut<dim> data_out;
     data_out.attach_dof_handler(dof_handler);
@@ -538,8 +606,8 @@ HydroFixedConfiguration<dim>::output_results(const unsigned int refinement_cycle
 
     std::ofstream output("solution-"
                          + dealii::Utilities::int_to_string(refinement_cycle, 2)
-                         + ".vtk");
-    data_out.write_vtk(output);
+                         + ".vtu");
+    data_out.write_vtu(output);
 }
 
 template <int dim>
@@ -600,7 +668,7 @@ void HydroFixedConfiguration<dim>::run()
 
 
     // triangulation.refine_global(4 - dim);
-    triangulation.refine_global(6);
+    triangulation.refine_global(5);
     std::cout << "Done refining mesh" << std::endl;
 
     for (unsigned int refinement_cycle = 0; refinement_cycle < 1;
