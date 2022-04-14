720c312e1d6194d5d43ed498a68e7d926db013ad
Lucas Myers <lucasmyers97@gmail.com>
2022-04-14 15:18:14 -0500

    Wrote example to show problem with efficient lagrange multiplier

--- app/examples/efficient_lagrange_multiplier.cpp
+++ app/examples/efficient_lagrange_multiplier.cpp
@@ -16,16 +16,25 @@ int main()
 {
     constexpr int order = 974;
     constexpr int dim = 3;
-    constexpr int num = 1000;
+    constexpr int num = 1;
 
     double alpha = 1.0;
-    double tol = 1e-13;
-    int max_iter = 10;
+    double tol = 1e-15;
+    int max_iter = 30;
+
+    double fudge = 0;
 
     dealii::Vector<double> Q_vec(msc::vec_dim<dim>);
     Q_vec[0] = -0.12599992319162331;
+    // Q_vec[0] = 0.25;
     Q_vec[1] = -0.23851658985824606;
+    // Q_vec[1] = 0.05;
+    // Q_vec[2] = 0.05;
+    Q_vec[2] = fudge;
+    // Q_vec[3] = -0.25;
     Q_vec[3] = 0.35103325652486811;
+    // Q_vec[4] = 0.05;
+    Q_vec[4] = fudge;
 
     // Q_vec[0] = 0.1;
     // Q_vec[1] = 0.06;
