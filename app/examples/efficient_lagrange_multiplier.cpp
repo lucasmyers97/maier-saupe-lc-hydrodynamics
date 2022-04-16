62ee221997dbab8cdd957ddc3731bbac47cd3b53
Lucas Myers <lucasmyers97@gmail.com>
2022-04-16 11:06:57 -0500

    Added a bunch of debugging stuff -- it's a mess

--- app/examples/efficient_lagrange_multiplier.cpp
+++ app/examples/efficient_lagrange_multiplier.cpp
@@ -19,81 +19,102 @@ int main()
     constexpr int num = 1;
 
     double alpha = 1.0;
-    double tol = 1e-15;
+    double tol = 1e-14;
     int max_iter = 30;
 
     double fudge = 0;
 
     dealii::Vector<double> Q_vec(msc::vec_dim<dim>);
-    Q_vec[0] = -0.12599992319162331;
-    // Q_vec[0] = 0.25;
-    Q_vec[1] = -0.23851658985824606;
-    // Q_vec[1] = 0.05;
-    // Q_vec[2] = 0.05;
-    Q_vec[2] = fudge;
-    // Q_vec[3] = -0.25;
-    Q_vec[3] = 0.35103325652486811;
-    // Q_vec[4] = 0.05;
-    Q_vec[4] = fudge;
+    // Q_vec[0] = -0.12599992319162331;
+    // // Q_vec[0] = 0.25;
+    // Q_vec[1] = -0.23851658985824606;
+    // // Q_vec[1] = 0.05;
+    // // Q_vec[2] = 0.05;
+    // Q_vec[2] = fudge;
+    // // Q_vec[3] = -0.25;
+    // Q_vec[3] = 0.35103325652486811;
+    // // Q_vec[4] = 0.05;
+    // Q_vec[4] = fudge;
 
-    // Q_vec[0] = 0.1;
-    // Q_vec[1] = 0.06;
-    // Q_vec[2] = 0.08;
-    // Q_vec[3] = -0.2;
-    // Q_vec[4] = 0.2;
+    // Q_vec[0] = -0.219537;
+    // Q_vec[1] = -0.0664242;
+    // Q_vec[2] = 0;
+    // Q_vec[3] = 0.444571;
+    // Q_vec[4] = 0;
+
 
     double Z_eff;
     dealii::Vector<double> Lambda_eff;
     dealii::FullMatrix<double> Jac_eff;
-
     LagrangeMultiplierEfficient<order, dim> lme(alpha, tol, max_iter);
-
-    auto start = std::chrono::high_resolution_clock::now();
-    for (unsigned int i = 0; i < num; ++i)
-        lme.invertQ(Q_vec);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto diff = end - start;
-
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
-              << std::endl;
-
-    Z_eff = lme.returnZ();
-    lme.returnLambda(Lambda_eff);
-    lme.returnJac(Jac_eff);
-
-    // std::cout << Z << std::endl;
-    // std::cout << Lambda << std::endl;
-    // Jac.print(std::cout, 10, 6);
-    // std::cout << std::endl;
 
     double Z;
     dealii::Vector<double> Lambda;
     dealii::LAPACKFullMatrix<double> Jac_lapack;
     LagrangeMultiplier<order, dim> lm(alpha, tol, max_iter);
 
-    start = std::chrono::high_resolution_clock::now();
+
     for (unsigned int i = 0; i < num; ++i)
+    {
+        double S = 0.6751;
+        double phi = (M_PI * static_cast<double>(i))
+            / static_cast<double>(num - 1);
+
+        // Q_vec[0] = 0.5 * S * (1.0 / 3.0 + std::cos(2 * phi));
+        // Q_vec[1] = 0.5 * S * std::sin(2 * phi);
+        // Q_vec[2] = 0;
+        // Q_vec[3] = 0.5 * S * (1.0 / 3.0 - std::cos(2 * phi));
+        // Q_vec[4] = 0;
+        Q_vec[0] = -0.277312;
+        Q_vec[1] = -0.0488917;
+        Q_vec[2] = 0;
+        Q_vec[3] = 0.502346;
+        Q_vec[4] = 0;
+
+        // std::cout << Q_vec << std::endl;
+
+        lme.invertQ(Q_vec);
         lm.invertQ(Q_vec);
-    end = std::chrono::high_resolution_clock::now();
-    diff = end - start;
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
-              << std::endl;
 
-    Z = lm.returnZ();
-    lm.returnLambda(Lambda);
-    lm.returnJac(Jac_lapack);
-    Jac_lapack.invert();
+        // auto start = std::chrono::high_resolution_clock::now();
+        // for (unsigned int i = 0; i < num; ++i)
+        //     lme.invertQ(Q_vec);
+        // auto end = std::chrono::high_resolution_clock::now();
+        // auto diff = end - start;
 
-    std::cout << (Z_eff - Z) << std::endl;
-    Lambda -= Lambda_eff;
-    std::cout << Lambda << std::endl;
+        // std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
+        //           << std::endl;
 
-    for (unsigned int i = 0; i < msc::vec_dim<dim>; ++i)
-        for (unsigned int j = 0; j < msc::vec_dim<dim>; ++j)
-            Jac_eff(i, j) -= Jac_lapack(i, j);
+        Z_eff = lme.returnZ();
+        lme.returnLambda(Lambda_eff);
+        lme.returnJac(Jac_eff);
 
-    Jac_eff.print(std::cout, 15, 6);
-    std::cout << std::endl;
+        // start = std::chrono::high_resolution_clock::now();
+        // for (unsigned int i = 0; i < num; ++i)
+        //     lm.invertQ(Q_vec);
+        // end = std::chrono::high_resolution_clock::now();
+        // diff = end - start;
+        // std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
+        //           << std::endl;
+
+        Z = lm.returnZ();
+        lm.returnLambda(Lambda);
+        lm.returnJac(Jac_lapack);
+        Jac_lapack.invert();
+
+        // std::cout << (Z_eff - Z) << std::endl;
+        Lambda -= Lambda_eff;
+        std::cout << Lambda.l2_norm() << std::endl;
+        std::cout << Lambda_eff << std::endl;
+
+        // for (unsigned int i = 0; i < msc::vec_dim<dim>; ++i)
+        //     for (unsigned int j = 0; j < msc::vec_dim<dim>; ++j)
+        //         Jac_eff(i, j) -= Jac_lapack(i, j);
+
+        // Jac_eff.print(std::cout, 15, 6);
+        // std::cout << std::endl;
+
+    }
 
     return 0;
 }
