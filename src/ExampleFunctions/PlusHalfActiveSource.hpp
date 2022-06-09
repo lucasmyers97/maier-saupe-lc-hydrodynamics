cf285ed743e8192f0c3d89c9e7153f28baa4697d
Lucas Myers <lucasmyers97@gmail.com>
2022-06-09 10:05:41 -0500

    Added more methods to the PlusHalfActiveSource class

--- src/ExampleFunctions/PlusHalfActiveSource.hpp
+++ src/ExampleFunctions/PlusHalfActiveSource.hpp
@@ -12,13 +12,21 @@ class PlusHalfActiveSource : public dealii::Function<dim>
 {
 public:
 
-  PlusHalfActiveSource() : dealii::Function<dim>(dim + 1) {}
+    PlusHalfActiveSource() : dealii::Function<dim>(dim + 1) {}
 
-  virtual double value(const dealii::Point<dim> &p,
-                       const unsigned int component = 0) const override;
+    virtual double value(const dealii::Point<dim> &p,
+                         const unsigned int component = 0) const override;
 
-  virtual void vector_value(const dealii::Point<dim> &p,
-                            dealii::Vector<double> &value) const override;
+    virtual void value_list(const std::vector<dealii::Point<dim>> &point_list,
+                            std::vector<double> &value_list,
+                            const unsigned int component = 0) const override;
+
+    virtual void vector_value(const dealii::Point<dim> &p,
+                              dealii::Vector<double> &value) const override;
+
+    virtual void vector_value_list(
+        const std::vector<dealii::Point<dim>> &point_list,
+        std::vector<dealii::Vector<double>> &value_list) const override;
 };
 
 template <int dim>
@@ -32,12 +40,42 @@ PlusHalfActiveSource<dim>::value(const dealii::Point<dim> &p /*p*/,
         return 0;
 }
 
+
+
+template <int dim>
+void PlusHalfActiveSource<dim>::
+    value_list(const std::vector<dealii::Point<dim>> &point_list,
+               std::vector<double> &value_list,
+               const unsigned int component) const
+{
+    if (component == 0)
+        for (unsigned int i = 0; i < point_list.size(); ++i)
+            value_list[i] = 1 / (2 * std::sqrt(point_list[0] * point_list[0]
+                                               + point_list[1] * point_list[1]));
+    else
+        for (unsigned int i = 0; i < point_list.size(); ++i)
+            value_list[i] = 0;
+}
+
+
+
 template <int dim>
 void PlusHalfActiveSource<dim>::vector_value(const dealii::Point<dim> &p,
-                                      dealii::Vector<double> &values) const
+                                             dealii::Vector<double> &values) const
 {
     for (unsigned int c = 0; c < this->n_components; ++c)
         values(c) = PlusHalfActiveSource<dim>::value(p, c);
+}
+
+
+
+template <int dim>
+void PlusHalfActiveSource<dim>::
+    vector_value_list(const std::vector<dealii::Point<dim>> &point_list,
+                      std::vector<dealii::Vector<double>> &value_list) const
+{
+    for (unsigned int i = 0; i < point_list.size(); ++i)
+        PlusHalfActiveSource<dim>::vector_value(point_list[i], value_list[i]);
 }
 
 #endif
