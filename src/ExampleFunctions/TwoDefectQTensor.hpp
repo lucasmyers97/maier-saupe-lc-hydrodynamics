e9f7e2caae4f2a196621aa6334ecb669a5ab4ad3
Lucas Myers <lucasmyers97@gmail.com>
2022-06-11 15:35:31 -0500

    Fixed mistake so that `TwoDefectQTensor` gives right values

--- src/ExampleFunctions/TwoDefectQTensor.hpp
+++ src/ExampleFunctions/TwoDefectQTensor.hpp
@@ -40,23 +40,25 @@ value(const dealii::Point<dim> &p) const
     dealii::Tensor<2, dim, double> Q;
 
     double theta = 0;
+    double m = 0;
     double S = 0;
     double r_coord = 0;
     for (unsigned int k = 0; k < centers.size(); ++k)
     {
-        theta = std::atan2(p[1] - centers[k][1], p[0] - centers[k][0]);
+        m = (k % 2) == 0 ? 1 : -1;
+        theta += m * std::atan2(p[1] - centers[k][1], p[0] - centers[k][0]);
         r_coord = std::sqrt((p[0] - centers[k][0]) * (p[0] - centers[k][0])
                             + (p[1] - centers[k][1]) * (p[1] - centers[k][1]));
-        S = 0.5 * (1 - std::exp(-r_coord / r));
-
-        Q[0][0] += 0.5 * S * ( (1.0/3.0) + std::cos(theta) );
-        Q[1][0] += 0.5 * S * std::sin(theta);
-        Q[0][1] += 0.5 * S * std::sin(theta);
-        Q[1][1] += 0.5 * S * ( (1.0/3.0) - std::cos(theta) );
-
-        if (dim == 3)
-            Q[2][2] += 0.5 * S * (-1.0/3.0);
+        S += 0.5 * (1 - std::exp(-r_coord / r));
     }
+
+    Q[0][0] = 0.5 * S * ( (1.0/3.0) + std::cos(theta) );
+    Q[1][0] = 0.5 * S * std::sin(theta);
+    Q[0][1] = 0.5 * S * std::sin(theta);
+    Q[1][1] = 0.5 * S * ( (1.0/3.0) - std::cos(theta) );
+
+    if (dim == 3)
+        Q[2][2] = 0.5 * S * (-1.0/3.0);
 
     return Q;
 }
@@ -72,26 +74,29 @@ value_list(const std::vector<dealii::Point<dim>> &points,
     {
 
         double theta = 0;
+        double m = 0;
         double S = 0;
         double r_coord = 0;
         for (unsigned int k = 0; k < centers.size(); ++k)
         {
-            theta = std::atan2(points[i][1] - centers[k][1],
-                               points[i][0] - centers[k][0]);
+            m = (k % 2) == 0 ? 1 : -1;
+            theta += m * std::atan2(points[i][1] - centers[k][1],
+                                    points[i][0] - centers[k][0]);
             r_coord = std::sqrt((points[i][0] - centers[k][0])
                                 * (points[i][0] - centers[k][0])
                                 + (points[i][1] - centers[k][1])
                                 * (points[i][1] - centers[k][1]));
-            S = 0.5 * (1 - std::exp(-r_coord / r));
+            S += 0.5 * (1 - std::exp(-r_coord / r));
 
-            values[i][0][0] += 0.5 * S * ((1.0 / 3.0) + std::cos(theta));
-            values[i][1][0] += 0.5 * S * std::sin(theta);
-            values[i][0][1] += 0.5 * S * std::sin(theta);
-            values[i][1][1] += 0.5 * S * ( (1.0/3.0) - std::cos(theta) );
-
-            if (dim == 3)
-                values[i][2][2] +=  0.5 * S * (-1.0 / 3.0);
         }
+
+        values[i][0][0] = 0.5 * S * ((1.0 / 3.0) + std::cos(theta));
+        values[i][1][0] = 0.5 * S * std::sin(theta);
+        values[i][0][1] = 0.5 * S * std::sin(theta);
+        values[i][1][1] = 0.5 * S * ( (1.0/3.0) - std::cos(theta) );
+
+        if (dim == 3)
+            values[i][2][2] =  0.5 * S * (-1.0 / 3.0);
     }
 }
 
