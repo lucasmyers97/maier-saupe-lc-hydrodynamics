767591613dd49fb738e71b487ace0f4c81c1d5da
Lucas Myers <lucasmyers97@gmail.com>
2022-06-13 13:07:49 -0500

    Added TwoDefectQTensor option to HydroFixedConfigurationSim
    Added ability to use TwoDefectQTensor Q-tensor configuration in
    HydroFixedConfigurationSim, and also modified some parameters.

--- app/simulations/HydroFixedConfigurationSim.cpp
+++ app/simulations/HydroFixedConfigurationSim.cpp
@@ -1,14 +1,18 @@
 #include "ExampleFunctions/PlusHalfQTensor.hpp"
+#include "ExampleFunctions/TwoDefectQTensor.hpp"
 #include "SimulationDrivers/BasicHydroDriver.hpp"
 
 #include <deal.II/base/function.h>
 #include <deal.II/base/tensor_function.h>
+#include <deal.II/base/point.h>
 
 #include <memory>
+#include <vector>
 
 #include "ExampleFunctions/PlusHalfQTensor.hpp"
 #include "ExampleFunctions/PeriodicMu2StressTensor.hpp"
 #include "ExampleFunctions/PeriodicQTensor.hpp"
+#include "ExampleFunctions/TwoDefectQTensor.hpp"
 
 int main(int ac, char* av[])
 {
@@ -16,12 +20,21 @@ int main(int ac, char* av[])
     {
         const int dim = 2;
         const unsigned int num_refines = 6;
+        const double left = -10.0;
+        const double right = 10.0;
 
         const double A = -0.064;
         const double B = -1.57;
         const double C = 1.29;
-        const double k = 13.0;
+        const double k = 0.5;
         const double eps = 0.01;
+
+        const double r = 1.0;
+        std::vector<dealii::Point<dim>> p(2);
+        p[0][0] = 1.0;
+        p[0][1] = 0.0;
+        p[1][0] = -1.0;
+        p[1][1] = 0.0;
 
         // std::unique_ptr<dealii::TensorFunction<2, dim, double>>
         //     stress_tensor = std::make_unique<PlusHalfQTensor<dim>>();
@@ -29,11 +42,15 @@ int main(int ac, char* av[])
             = std::make_unique<PeriodicMu2StressTensor<dim>>(A, B, C, k, eps);
         // std::unique_ptr<dealii::TensorFunction<2, dim, double>> Q_tensor
         //     = std::make_unique<PeriodicQTensor<dim>>(k, eps);
+        // std::unique_ptr<dealii::TensorFunction<2, dim, double>> Q_tensor =
+        //     std::make_unique<TwoDefectQTensor<dim>>(p, r);
         std::unique_ptr<dealii::TensorFunction<2, dim, double>> Q_tensor =
             std::make_unique<dealii::ZeroTensorFunction<2, dim>>();
         BasicHydroDriver<dim> hydro_driver(std::move(stress_tensor),
                                            std::move(Q_tensor),
-                                           num_refines);
+                                           num_refines,
+                                           left,
+                                           right);
         hydro_driver.run();
     }
     catch (std::exception &exc)
