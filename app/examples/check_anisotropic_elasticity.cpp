879737321619977667ab8cc516a396e2f4da9007
Lucas Myers <lucasmyers97@gmail.com>
2022-08-22 11:56:28 -0500

    Outputted all elastic boundary terms

--- app/examples/check_anisotropic_elasticity.cpp
+++ app/examples/check_anisotropic_elasticity.cpp
@@ -660,6 +660,238 @@ void assemble_bE1(const dealii::DoFHandler<dim> &dof_handler,
 
 
 template <int dim>
+void assemble_bE2(const dealii::DoFHandler<dim> &dof_handler,
+                 const dealii::AffineConstraints<double> &constraints,
+                 const LA::MPI::Vector &solution,
+                 LA::MPI::Vector &system_rhs)
+{
+    const dealii::FESystem<dim> &fe = dof_handler.get_fe();
+
+    dealii::QGauss<dim> quadrature_formula(fe.degree + 1);
+    dealii::QGauss<dim - 1> face_quadrature_formula(fe.degree + 1);
+
+    system_rhs = 0;
+
+    dealii::FEValues<dim> fe_values(fe,
+                                    quadrature_formula,
+                                    dealii::update_values
+                                    | dealii::update_gradients
+                                    | dealii::update_JxW_values);
+    dealii::FEFaceValues<dim> fe_face_values(fe,
+                                             face_quadrature_formula,
+                                             dealii::update_values
+                                             | dealii::update_quadrature_points 
+                                             | dealii::update_normal_vectors 
+                                             | dealii::update_JxW_values);
+
+    const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+    const unsigned int n_q_points = quadrature_formula.size();
+    const unsigned int n_face_q_points = face_quadrature_formula.size();
+
+    dealii::Vector<double> cell_rhs(dofs_per_cell);
+
+    std::vector<std::vector<dealii::Tensor<1, dim>>>
+        dQ(n_q_points,
+           std::vector<dealii::Tensor<1, dim, double>>(fe.components));
+    std::vector<dealii::Vector<double>>
+        Q_vec(n_q_points, dealii::Vector<double>(fe.components));
+    std::vector<dealii::Vector<double>>
+        Q0_vec(n_q_points, dealii::Vector<double>(fe.components));
+
+    dealii::Vector<double> Lambda_vec(fe.components);
+    dealii::FullMatrix<double> dLambda_dQ(fe.components, fe.components);
+
+    std::vector<dealii::types::global_dof_index>
+        local_dof_indices(dofs_per_cell);
+
+    for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+        if (!(cell->is_locally_owned()))
+            continue;
+
+        cell_rhs = 0;
+
+        fe_values.reinit(cell);
+        fe_values.get_function_gradients(solution, dQ);
+        fe_values.get_function_values(solution, Q_vec);
+        for (const auto &face : cell->face_iterators())
+            if (face->at_boundary())
+            {
+                fe_face_values.reinit(cell, face);
+ 
+                for (unsigned int q = 0; q < n_face_q_points; ++q)
+                {
+                    for (unsigned int i = 0; i < dofs_per_cell; ++i)
+                    {
+                        const unsigned int component_i =
+                            fe.system_to_component_index(i).first;
+
+                        if (component_i == 0)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*dQ[q][0][0] + fe_face_values.normal_vector(q)[1]*dQ[q][1][0] + fe_face_values.normal_vector(q)[2]*dQ[q][2][0])
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 1)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*dQ[q][0][1] + fe_face_values.normal_vector(q)[0]*dQ[q][1][0] + fe_face_values.normal_vector(q)[1]*dQ[q][1][1] + fe_face_values.normal_vector(q)[1]*dQ[q][3][0] + fe_face_values.normal_vector(q)[2]*dQ[q][2][1] + fe_face_values.normal_vector(q)[2]*dQ[q][4][0])
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 2)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*dQ[q][2][0] + fe_face_values.normal_vector(q)[1]*dQ[q][4][0] - fe_face_values.normal_vector(q)[2]*(dQ[q][0][0] + dQ[q][3][0]))
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 3)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*dQ[q][1][1] + fe_face_values.normal_vector(q)[1]*dQ[q][3][1] + fe_face_values.normal_vector(q)[2]*dQ[q][4][1])
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 4)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*dQ[q][2][1] + fe_face_values.normal_vector(q)[1]*dQ[q][4][1] - fe_face_values.normal_vector(q)[2]*(dQ[q][0][1] + dQ[q][3][1]))
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);   
+                    }
+                }
+            }
+
+        cell->get_dof_indices(local_dof_indices);
+        constraints.distribute_local_to_global(cell_rhs,
+                                               local_dof_indices,
+                                                system_rhs);
+    }
+    system_rhs.compress(dealii::VectorOperation::add);
+}
+
+
+
+template <int dim>
+void assemble_bE31(const dealii::DoFHandler<dim> &dof_handler,
+                 const dealii::AffineConstraints<double> &constraints,
+                 const LA::MPI::Vector &solution,
+                 LA::MPI::Vector &system_rhs)
+{
+    const dealii::FESystem<dim> &fe = dof_handler.get_fe();
+
+    dealii::QGauss<dim> quadrature_formula(fe.degree + 1);
+    dealii::QGauss<dim - 1> face_quadrature_formula(fe.degree + 1);
+
+    system_rhs = 0;
+
+    dealii::FEValues<dim> fe_values(fe,
+                                    quadrature_formula,
+                                    dealii::update_values
+                                    | dealii::update_gradients
+                                    | dealii::update_JxW_values);
+    dealii::FEFaceValues<dim> fe_face_values(fe,
+                                             face_quadrature_formula,
+                                             dealii::update_values
+                                             | dealii::update_quadrature_points 
+                                             | dealii::update_normal_vectors 
+                                             | dealii::update_JxW_values);
+
+    const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+    const unsigned int n_q_points = quadrature_formula.size();
+    const unsigned int n_face_q_points = face_quadrature_formula.size();
+
+    dealii::Vector<double> cell_rhs(dofs_per_cell);
+
+    std::vector<std::vector<dealii::Tensor<1, dim>>>
+        dQ(n_q_points,
+           std::vector<dealii::Tensor<1, dim, double>>(fe.components));
+    std::vector<dealii::Vector<double>>
+        Q_vec(n_q_points, dealii::Vector<double>(fe.components));
+    std::vector<dealii::Vector<double>>
+        Q0_vec(n_q_points, dealii::Vector<double>(fe.components));
+
+    dealii::Vector<double> Lambda_vec(fe.components);
+    dealii::FullMatrix<double> dLambda_dQ(fe.components, fe.components);
+
+    std::vector<dealii::types::global_dof_index>
+        local_dof_indices(dofs_per_cell);
+
+    for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+        if (!(cell->is_locally_owned()))
+            continue;
+
+        cell_rhs = 0;
+
+        fe_values.reinit(cell);
+        fe_values.get_function_gradients(solution, dQ);
+        fe_values.get_function_values(solution, Q_vec);
+        for (const auto &face : cell->face_iterators())
+            if (face->at_boundary())
+            {
+                fe_face_values.reinit(cell, face);
+ 
+                for (unsigned int q = 0; q < n_face_q_points; ++q)
+                {
+                    for (unsigned int i = 0; i < dofs_per_cell; ++i)
+                    {
+                        const unsigned int component_i =
+                            fe.system_to_component_index(i).first;
+
+                        if (component_i == 0)
+                            cell_rhs(i) +=
+                                (
+                                 ((2*fe_face_values.normal_vector(q)[0]*Q_vec[q][0]*dQ[q][0][0] + fe_face_values.normal_vector(q)[0]*Q_vec[q][0]*dQ[q][3][0] + 2*fe_face_values.normal_vector(q)[0]*Q_vec[q][1]*dQ[q][0][1] + fe_face_values.normal_vector(q)[0]*Q_vec[q][1]*dQ[q][3][1] + 2*fe_face_values.normal_vector(q)[1]*Q_vec[q][1]*dQ[q][0][0] + fe_face_values.normal_vector(q)[1]*Q_vec[q][1]*dQ[q][3][0] + 2*fe_face_values.normal_vector(q)[1]*Q_vec[q][3]*dQ[q][0][1] + fe_face_values.normal_vector(q)[1]*Q_vec[q][3]*dQ[q][3][1] + 2*fe_face_values.normal_vector(q)[2]*Q_vec[q][2]*dQ[q][0][0] + fe_face_values.normal_vector(q)[2]*Q_vec[q][2]*dQ[q][3][0] + 2*fe_face_values.normal_vector(q)[2]*Q_vec[q][4]*dQ[q][0][1] + fe_face_values.normal_vector(q)[2]*Q_vec[q][4]*dQ[q][3][1])
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 1)
+                            cell_rhs(i) +=
+                                (
+                                 (2*(fe_face_values.normal_vector(q)[0]*(Q_vec[q][0]*dQ[q][1][0] + Q_vec[q][1]*dQ[q][1][1]) + fe_face_values.normal_vector(q)[1]*(Q_vec[q][1]*dQ[q][1][0] + Q_vec[q][3]*dQ[q][1][1]) + fe_face_values.normal_vector(q)[2]*(Q_vec[q][2]*dQ[q][1][0] + Q_vec[q][4]*dQ[q][1][1]))
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 2)
+                            cell_rhs(i) +=
+                                (
+                                 (2*(fe_face_values.normal_vector(q)[0]*(Q_vec[q][0]*dQ[q][2][0] + Q_vec[q][1]*dQ[q][2][1]) + fe_face_values.normal_vector(q)[1]*(Q_vec[q][1]*dQ[q][2][0] + Q_vec[q][3]*dQ[q][2][1]) + fe_face_values.normal_vector(q)[2]*(Q_vec[q][2]*dQ[q][2][0] + Q_vec[q][4]*dQ[q][2][1]))
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 3)
+                            cell_rhs(i) +=
+                                (
+                                 ((fe_face_values.normal_vector(q)[0]*Q_vec[q][0]*dQ[q][0][0] + 2*fe_face_values.normal_vector(q)[0]*Q_vec[q][0]*dQ[q][3][0] + fe_face_values.normal_vector(q)[0]*Q_vec[q][1]*dQ[q][0][1] + 2*fe_face_values.normal_vector(q)[0]*Q_vec[q][1]*dQ[q][3][1] + fe_face_values.normal_vector(q)[1]*Q_vec[q][1]*dQ[q][0][0] + 2*fe_face_values.normal_vector(q)[1]*Q_vec[q][1]*dQ[q][3][0] + fe_face_values.normal_vector(q)[1]*Q_vec[q][3]*dQ[q][0][1] + 2*fe_face_values.normal_vector(q)[1]*Q_vec[q][3]*dQ[q][3][1] + fe_face_values.normal_vector(q)[2]*Q_vec[q][2]*dQ[q][0][0] + 2*fe_face_values.normal_vector(q)[2]*Q_vec[q][2]*dQ[q][3][0] + fe_face_values.normal_vector(q)[2]*Q_vec[q][4]*dQ[q][0][1] + 2*fe_face_values.normal_vector(q)[2]*Q_vec[q][4]*dQ[q][3][1])
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                        else if (component_i == 4)
+                            cell_rhs(i) +=
+                                (
+                                 (2*(fe_face_values.normal_vector(q)[0]*(Q_vec[q][0]*dQ[q][4][0] + Q_vec[q][1]*dQ[q][4][1]) + fe_face_values.normal_vector(q)[1]*(Q_vec[q][1]*dQ[q][4][0] + Q_vec[q][3]*dQ[q][4][1]) + fe_face_values.normal_vector(q)[2]*(Q_vec[q][2]*dQ[q][4][0] + Q_vec[q][4]*dQ[q][4][1]))
+                                  * fe_face_values.shape_value(i, q))
+                                )
+                                * fe_face_values.JxW(q);
+                    }
+                }
+            }
+
+        cell->get_dof_indices(local_dof_indices);
+        constraints.distribute_local_to_global(cell_rhs,
+                                               local_dof_indices,
+                                                system_rhs);
+    }
+    system_rhs.compress(dealii::VectorOperation::add);
+}
+
+
+
+template <int dim>
 void output_Q_components(const MPI_Comm &mpi_communicator,
                          const dealii::DoFHandler<dim> &dof_handler,
                          const LA::MPI::Vector &system_rhs,
@@ -744,21 +976,27 @@ int main(int ac, char* av[])
         LA::MPI::Vector E1;
         LA::MPI::Vector bE1;
         LA::MPI::Vector E2;
+        LA::MPI::Vector bE2;
         LA::MPI::Vector E31;
+        LA::MPI::Vector bE31;
         LA::MPI::Vector E32;
 
         R1.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
         E1.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
         bE1.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
         E2.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
+        bE2.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
         E31.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
+        bE31.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
         E32.reinit(dof_handler.locally_owned_dofs(), mpi_communicator);
 
         assemble_R1(dof_handler, constraints, solution, R1);
         assemble_E1(dof_handler, constraints, solution, E1);
         assemble_bE1(dof_handler, constraints, solution, bE1);
         assemble_E2(dof_handler, constraints, solution, E2);
+        assemble_bE2(dof_handler, constraints, solution, bE2);
         assemble_E31(dof_handler, constraints, solution, E31);
+        assemble_bE31(dof_handler, constraints, solution, bE31);
         assemble_E32(dof_handler, constraints, solution, E32);
 
         output_Q_components(mpi_communicator,
@@ -791,10 +1029,24 @@ int main(int ac, char* av[])
                             0);
         output_Q_components(mpi_communicator,
                             dof_handler,
+                            bE2,
+                            tria,
+                            std::string("./"),
+                            std::string("bE2_config"),
+                            0);
+        output_Q_components(mpi_communicator,
+                            dof_handler,
                             E31,
                             tria,
                             std::string("./"),
                             std::string("E31_config"),
+                            0);
+        output_Q_components(mpi_communicator,
+                            dof_handler,
+                            bE31,
+                            tria,
+                            std::string("./"),
+                            std::string("bE31_config"),
                             0);
         output_Q_components(mpi_communicator,
                             dof_handler,
