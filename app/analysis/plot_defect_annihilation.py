cac3db9499222eb6dac1506bd2da08bc205613c2
Lucas Myers <lucasmyers97@gmail.com>
2022-09-23 15:03:26 -0500

    Updated defect annihilation analysis script
    Used epsilon instead of L3, added t0 for where to start plot, added
    default value to output_folder (just save where data is coming from),
            also separater by defect charge instead of using some threshold
            (because now I'm saving the charge)

--- app/analysis/plot_defect_annihilation.py
+++ app/analysis/plot_defect_annihilation.py
@@ -29,29 +29,34 @@ def get_filenames():
                         help='folder which will hold output plots')
     parser.add_argument('--plot_filename', dest='plot_filename',
                         help='filename of regularly-scaled x vs. t plot')
-    parser.add_argument('--log_plot_filename', dest='log_plot_filename',
-                        help='filename of log-scaled x vs. t plot')
     parser.add_argument('--squared_filename', dest='squared_filename',
                         help='filename of x vs. t^2 plot')
-    parser.add_argument('--L3', dest='L3',
-                        help='L3 value associated with annihilation')
+    parser.add_argument('--eps', dest='eps',
+                        help='epsilon value associated with annihilation')
+    parser.add_argument('--t0', dest='t0',
+                        type=int,
+                        default=0,
+                        help='time to start annihilation plots at')
     parser.add_argument('--threshold', dest='threshold',
                         help='x-value at which to separate two defects')
     args = parser.parse_args()
 
+    output_folder = args.output_folder
+    if not output_folder:
+        output_folder = args.data_folder
+
     defect_filename = os.path.join(args.data_folder, args.defect_filename)
-    plot_filename = os.path.join(args.output_folder, args.plot_filename)
-    log_plot_filename = os.path.join(args.output_folder, args.log_plot_filename)
-    squared_filename = os.path.join(args.output_folder, args.squared_filename)
+    plot_filename = os.path.join(output_folder, args.plot_filename)
+    squared_filename = os.path.join(output_folder, args.squared_filename)
 
-    return plot_filename, log_plot_filename, defect_filename, squared_filename, args.L3, float(args.threshold)
-
+    return plot_filename, defect_filename, squared_filename, args.eps, args.t0
 
 
-def separate_defects(t, x, threshold=0):
 
-    pos_idx = x > threshold
-    neg_idx = x <= threshold
+def separate_defects(t, x, charge):
+
+    pos_idx = charge > 0
+    neg_idx = charge < 0
 
     x_pos = x[pos_idx]
     x_neg = x[neg_idx]
@@ -70,6 +75,15 @@ def order_points(t, x):
 
 
 
+def get_annihilation_point(t, x):
+
+    t_f = t[0][-1]
+    x_f = (x[0][-1] + x[1][-1]) / 2
+
+    return t_f, x_f
+
+
+
 def fit_sqrt(t, x):
 
     if x[0] > 0:
@@ -84,23 +98,24 @@ def fit_sqrt(t, x):
 
 def main():
 
-    plot_filename, log_plot_filename, defect_filename, squared_filename, L3, threshold = get_filenames()
+    plot_filename, defect_filename, squared_filename, eps, t0 = get_filenames()
     
     file = h5py.File(defect_filename)
-    # t = np.array(file['defect']['t'][:])
-    # x = np.array(file['defect']['x'][:])
     t = np.array(file['t'][:])
     x = np.array(file['x'][:])
+    charge = np.array(file['charge'][:])
 
-    t, x = separate_defects(t, x, threshold)
+    t, x = separate_defects(t, x, charge)
     for i in range(2):
         t[i], x[i] = order_points(t[i], x[i])
 
-    offset = 0
-    t[0] = t[0][offset:]
-    t[1] = t[1][offset:]
-    x[0] = x[0][offset:]
-    x[1] = x[1][offset:]
+    t[0] = t[0][t0:]
+    t[1] = t[1][t0:]
+    x[0] = x[0][t0:]
+    x[1] = x[1][t0:]
+
+    t_f, x_f = get_annihilation_point(t, x)
+    print("Annihilation point (t_f, x_f) is: ({}, {})".format(t_f, x_f))
 
     # A = fit_sqrt(t[0], x[0])
     # B = fit_sqrt(t[1], x[1])
@@ -118,7 +133,7 @@ def main():
     #         label=r'$A_0 = {:.2E}, A_1 = {:.2E}$'.format(B[0], B[1]))
     
     # ax.set_title(r"$\pm 1/2$ defect annihilation, $x = A_0 \sqrt{A_1 - t}$")
-    ax.set_title(r"$\pm 1/2$ defect annihilation, $L_3 = {}$".format(L3))
+    ax.set_title(r"$\pm 1/2$ defect annihilation, $\epsilon = {}$".format(eps))
     ax.set_xlabel(r"$t$")
     ax.set_ylabel(r"$x$")
     ax.legend(fontsize=8)
@@ -131,7 +146,7 @@ def main():
     ax.plot(t[0], x[0]**2, label="+1/2 defect")
     ax.plot(t[1], x[1]**2, label="-1/2 defect")
 
-    ax.set_title(r"$\pm 1/2$ defect annihilation, $L_3 = {}$".format(L3))
+    ax.set_title(r"$\pm 1/2$ defect annihilation, $\epsilon = {}$".format(eps))
     ax.set_xlabel(r"$t$")
     ax.set_ylabel(r"$x^2$")
     ax.legend(fontsize=8)
