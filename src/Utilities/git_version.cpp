91d8de511cd162660ac3f3d0818c07f0c3e3c792
Lucas Myers <lucasmyers97@gmail.com>
2022-11-11 14:33:08 -0600

    Wrote some code to put git hash in code

---
+++ src/Utilities/git_version.cpp
@@ -0,0 +1,4 @@
+#include "git_version.hpp"
+#include <string>
+
+const std::string kGitHash = std::string("8a33704");
---
+++ src/Utilities/git_version.hpp
@@ -0,0 +1,8 @@
+#ifndef GIT_VERSION_HPP
+#define GIT_VERSION_HPP
+
+#include <string>
+
+extern const std::string kGitHash;
+
+#endif // GIT_VERSION_HPP
