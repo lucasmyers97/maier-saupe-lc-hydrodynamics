ed66393251104ff700a2fde497dc0d59532053e8
Lucas Myers <lucasmyers97@gmail.com>
2022-11-16 15:51:12 -0600

    Changed order of indices when writing Q-tensors

--- app/analysis/generate_periodic_Q_tensors.py
+++ app/analysis/generate_periodic_Q_tensors.py
@@ -50,10 +50,10 @@ def main():
      filename, dataset_name) = get_commandline_args()
 
     x = np.linspace(limits[0], limits[1], n_points)
-    Q = np.zeros((vec_dim, n_points))
-    Q[0, :] = S * (2 / 3)
-    Q[1, :] = S * eps * np.sin(k * x)
-    Q[3, :] = -S * (1 / 3)
+    Q = np.zeros((n_points, vec_dim))
+    Q[:, 0] = S * (2 / 3)
+    Q[:, 1] = S * eps * np.sin(k * x)
+    Q[:, 3] = -S * (1 / 3)
 
     with h5py.File(filename, "w") as f:
         dset = f.create_dataset(dataset_name, data=Q)
