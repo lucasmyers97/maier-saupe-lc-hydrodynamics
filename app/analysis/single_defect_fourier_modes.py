a6f681c79ea38a0a3f34b4d9cb0241814d006069
Lucas Myers <lucasmyers97@gmail.com>
2023-01-23 11:03:08 -0600

    Restrucured and added parameter to bound polynomial plots

--- app/analysis/single_defect_fourier_modes.py
+++ app/analysis/single_defect_fourier_modes.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+from scipy.optimize import curve_fit
+
 plt.style.use('science')
 
 mpl.rcParams['figure.dpi'] = 300
@@ -35,6 +37,10 @@ def get_commandline_args():
     parser.add_argument('--timestep_key',
                         dest='timestep_key',
                         help='key name of timestep in hdf5 file')
+    parser.add_argument('--r0',
+                        dest='r0',
+                        type=float,
+                        help='cutoff for polynomial behavior of Fourier modes')
     args = parser.parse_args()
 
     input_filename = os.path.join(args.data_folder, args.input_filename)
@@ -44,13 +50,85 @@ def get_commandline_args():
     else:
         plot_prefix = os.path.join(args.data_folder, args.plot_prefix)
 
-    return input_filename, plot_prefix, args.n_modes, args.timestep_key
+    return input_filename, plot_prefix, args.n_modes, args.timestep_key, args.r0
+
+
+
+def get_fourier_coeffs(X, n_modes):
+
+    An_X = np.zeros((X.shape[1], n_modes))
+    Bn_X = np.zeros((X.shape[1], n_modes))
+
+    for i in range(X.shape[1]):
+
+        # X at each fixed R
+        x = X[:, i]
+
+        FT_x = np.fft.rfft(x)
+        N = x.shape[0]
+
+        # get sin and cos coefficients from DFT
+        An_x = FT_x.real / N
+        Bn_x = FT_x.imag / N
+
+        # cos modes
+        for j in range(n_modes):
+            An_X[i, j] = An_x[j]
+
+        # sin modes
+        for j in range(1, n_modes):
+            Bn_X[i, j] = Bn_x[j]
+
+    return An_X, Bn_X
+
+
+
+def plot_fourier_coeffs(Cn, r, n_modes, title, include_first=True):
+
+    if include_first:
+        start = 0
+    else:
+        start = 1
+
+    # make plots
+    fig_Cn, ax_Cn = plt.subplots()
+    for j in range(start, n_modes):
+        ax_Cn.plot(r, Cn[:, j], label="k = {}".format(j))
+
+    ax_Cn.set_xlabel("radius")
+    ax_Cn.set_ylabel("Fourier amplitude")
+    ax_Cn.set_title(title)
+    ax_Cn.legend()
+
+    fig_Cn.tight_layout()
+    
+    return fig_Cn, ax_Cn
+
+
+
+def plot_two_fourier_coeffs(Cn, r, ylabel1, ylabel2):
+
+    color = 'tab:red'
+    fig_Cn, ax_Cn = plt.subplots()
+    ax_Cn.plot(r, Cn[:, 0], color=color)
+    ax_Cn.set_xlabel(r'$r / \xi$')
+    ax_Cn.set_ylabel(ylabel1, color=color)
+    ax_Cn.tick_params(axis='y', labelcolor=color)
+
+    color = 'tab:blue'
+    ax2_Cn = ax_Cn.twinx()
+    ax2_Cn.plot(r, -Cn[:, 1], color=color)
+    ax2_Cn.set_ylabel(ylabel2, color=color)
+    ax2_Cn.tick_params(axis='y', labelcolor=color)
+    fig_Cn.tight_layout()
+
+    return fig_Cn, ax_Cn, ax2_Cn
 
 
 
 def main():
 
-    input_filename, plot_prefix, n_modes, timestep_key = get_commandline_args()
+    input_filename, plot_prefix, n_modes, timestep_key, r0 = get_commandline_args()
 
     file = h5py.File(input_filename)
     grp = file[timestep_key]
@@ -70,123 +148,100 @@ def main():
     S = S.reshape(point_dims)
     P = P.reshape(point_dims)
 
-    An_q1 = np.zeros((R.shape[1], n_modes))
-    Bn_q1 = np.zeros((R.shape[1], n_modes))
-    An_q2 = np.zeros((R.shape[1], n_modes))
-    Bn_q2 = np.zeros((R.shape[1], n_modes))
-    An_Gamma = np.zeros((R.shape[1], n_modes))
+    An_q1, Bn_q1 = get_fourier_coeffs(q1, n_modes)
+    An_q2, Bn_q2 = get_fourier_coeffs(q2, n_modes)
+    An_Gamma, _ = get_fourier_coeffs(S - P, n_modes)
 
-    for i in range(R.shape[1]):
-
-        # values for fixed r, varied theta
-        s = S[:, i]
-        p = P[:, i]
-        gamma = s - p
-        q1_r = q1[:, i]
-        q2_r = q2[:, i]
-
-        FT_q1_r = np.fft.rfft(q1_r)
-        FT_q2_r = np.fft.rfft(q2_r)
-        FT_gamma = np.fft.rfft(gamma)
-
-        # change discrete fourier into sin/cos numbered fourier modes
-        N = q1_r.shape[0]
-        An_q1_r = FT_q1_r.real / N
-        Bn_q1_r = FT_q1_r.imag / N
-        An_q2_r = FT_q2_r.real / N
-        Bn_q2_r = FT_q2_r.imag / N
-        An_gamma = FT_gamma.real / N
-
-        # cos modes
-        for j in range(n_modes):
-            An_q1[i, j] = An_q1_r[j]
-            An_q2[i, j] = An_q2_r[j]
-            An_Gamma[i, j] = An_gamma[j]
-
-        # sin modes
-        for j in range(1, n_modes):
-            Bn_q1[i, j] = Bn_q1_r[j]
-            Bn_q2[i, j] = Bn_q2_r[j]
-
-    # make plots
-    fig_An_q1, ax_An_q1 = plt.subplots()
-    fig_Bn_q1, ax_Bn_q1 = plt.subplots()
-    fig_An_q2, ax_An_q2 = plt.subplots()
-    fig_Bn_q2, ax_Bn_q2 = plt.subplots()
-    for j in range(n_modes):
-        ax_An_q1.plot(R[0, :], An_q1[:, j], label="k = {}".format(j))
-        ax_An_q2.plot(R[0, :], An_q2[:, j], label="k = {}".format(j))
-
-    for j in range(1, n_modes):
-        ax_Bn_q1.plot(R[0, :], Bn_q1[:, j], label="k = {}".format(j))
-        ax_Bn_q2.plot(R[0, :], Bn_q2[:, j], label="k = {}".format(j))
-
-    ax_An_q1.set_xlabel("radius")
-    ax_An_q1.set_ylabel("Fourier amplitude")
-    ax_An_q1.set_title(r'$\cos$ Fourier coefficients for $q_1$')
-    ax_An_q1.legend()
-
-    ax_Bn_q1.set_xlabel("radius")
-    ax_Bn_q1.set_ylabel("Fourier amplitude")
-    ax_Bn_q1.set_title(r'$\sin$ Fourier coefficients for $q_1$')
-    ax_Bn_q1.legend()
-
-    ax_An_q2.set_xlabel("radius")
-    ax_An_q2.set_ylabel("Fourier amplitude")
-    ax_An_q2.set_title(r'$\cos$ Fourier coefficients for $q_2$')
-    ax_An_q2.legend()
-
-    ax_Bn_q2.set_xlabel("radius")
-    ax_Bn_q2.set_ylabel("Fourier amplitude")
-    ax_Bn_q2.set_title(r'$\sin$ Fourier coefficients for $q_2$')
-    ax_Bn_q2.legend()
-
-    fig_An_q1.tight_layout()
-    fig_Bn_q1.tight_layout()
-    fig_An_q2.tight_layout()
-    fig_Bn_q2.tight_layout()
+    # plot first two modes
+    (fig_An_q1, 
+     ax1_An_q1, 
+     ax2_An_q1) = plot_two_fourier_coeffs(An_q1, 
+                                          R[0, :],
+                                          ylabel1=r'$q_1^{(0)} (r)$',
+                                          ylabel2=r'$-q_1^{(1)} (r)$')
+    (fig_An_q2, 
+     ax1_An_q2, 
+     ax2_An_q2) = plot_two_fourier_coeffs(An_q2, 
+                                          R[0, :],
+                                          ylabel1=r'$q_2^{(0)} (r)$',
+                                          ylabel2=r'$-q_2^{(1)} (r)$')
+    (fig_An_Gamma, 
+     ax1_An_Gamma, 
+     ax2_An_Gamma) = plot_two_fourier_coeffs(An_Gamma, 
+                                             R[0, :],
+                                             ylabel1=r'$\Gamma_0 (r)$',
+                                             ylabel2=r'$-\Gamma_1 (r)$')
 
     fig_An_q1.savefig(plot_prefix + "An_q1.png")
-    fig_Bn_q1.savefig(plot_prefix + "Bn_q1.png")
     fig_An_q2.savefig(plot_prefix + "An_q2.png")
-    fig_Bn_q2.savefig(plot_prefix + "Bn_q2.png")
-
-    color = 'tab:red'
-    fig_An_Gamma, ax_An_Gamma = plt.subplots()
-    ax_An_Gamma.plot(R[0, :], An_Gamma[:, 0], color=color)
-    ax_An_Gamma.set_xlabel(r'$r / \xi$')
-    ax_An_Gamma.set_ylabel(r'$\Gamma_0 (r)$', color=color)
-    ax_An_Gamma.tick_params(axis='y', labelcolor=color)
-
-    color = 'tab:blue'
-    ax2_An_Gamma = ax_An_Gamma.twinx()
-    ax2_An_Gamma.plot(R[0, :], -An_Gamma[:, 1], color=color)
-    ax2_An_Gamma.set_ylabel(r'$-\Gamma_1 (r)$', color=color)
-    ax2_An_Gamma.tick_params(axis='y', labelcolor=color)
-    ax2_An_Gamma.set_ylim([-0.001, 0.025])
-    fig_An_Gamma.tight_layout()
-
-    color = 'tab:red'
-    fig_An_Gamma, ax_An_Gamma = plt.subplots()
-    ax_An_Gamma.plot(R[0, :], An_Gamma[:, 0], color=color)
-    ax_An_Gamma.set_xlabel(r'$r / \xi$')
-    ax_An_Gamma.set_ylabel(r'$\Gamma_0 (r)$', color=color)
-    ax_An_Gamma.tick_params(axis='y', labelcolor=color)
-    ax_An_Gamma.set_xlim([-0.1, 0.5])
-    ax_An_Gamma.set_ylim([-6e-3, 3.5e-2])
-
-    color = 'tab:blue'
-    ax2_An_Gamma = ax_An_Gamma.twinx()
-    ax2_An_Gamma.plot(R[0, :], (-(An_Gamma[:, 1] - An_Gamma[0, 1] - 1e-8))**0.5, color=color)
-    ax2_An_Gamma.plot(R[0, :], (-(An_Gamma[:, 1] - An_Gamma[0, 1] - 1e-8))**(1/3), color='tab:green')
-    ax2_An_Gamma.set_ylabel(r'$-\Gamma_1 (r)$', color=color)
-    ax2_An_Gamma.tick_params(axis='y', labelcolor=color)
-    # ax2_An_Gamma.set_ylim([-4e-4, 2e-3])
-    ax2_An_Gamma.set_ylim([-4e-4, 8e-3])
-    ax2_An_Gamma.set_xlim([-0.1, 0.5])
-    fig_An_Gamma.tight_layout()
+    fig_An_Gamma.savefig(plot_prefix + "An_Gamma.png")
 
     plt.show()
+
+    # Look at small r regime
+    r_small_idx = R[0, :] < r0
+    r_small = R[0, r_small_idx]
+
+    A0_q1_small = An_q1[r_small_idx, 0]
+    A1_q1_small = An_q1[r_small_idx, 1]
+    A0_q1_small -= np.min(A0_q1_small) - 1e-8
+    A1_q1_small -= np.max(A1_q1_small) + 1e-8
+
+    # plt.figure()
+    # plt.plot(r_small, A0_q1_small)
+    # plt.figure()
+    # plt.plot(r_small, A1_q1_small)
+
+    A0_fig_log, A0_ax_log = plt.subplots()
+    A0_ax_log.plot(np.log(r_small), np.log(A0_q1_small))
+    A0_ax_log.set_xlabel(r'$\log(r)$')
+    A0_ax_log.set_ylabel(r'$\log(A_{q_1}^{(0)})$')
+    A0_ax_log.set_title(r'$\log(A_{q_1}^{(0)})$ for small $r$')
+    A0_fig_log.tight_layout()
+
+    A1_fig_log, A1_ax_log = plt.subplots()
+    A1_ax_log.plot(np.log(r_small), np.log(-A1_q1_small))
+    A1_ax_log.set_xlabel(r'$\log(r)$')
+    A1_ax_log.set_ylabel(r'$\log(A_{q_1}^{(1)})$')
+    A1_ax_log.set_title(r'$\log(A_{q_1}^{(1)})$ for small $r$')
+    A1_fig_log.tight_layout()
+
+    A0_fig, A0_ax = plt.subplots()
+    A0_ax.plot(np.log(r_small), np.log(A0_q1_small))
+    A0_ax.set_xlabel(r'$r$')
+    A0_ax.set_ylabel(r'$A_{q_1}^{(0)}$')
+    A0_ax.set_title(r'$A_{q_1}^{(0)}$ for small $r$')
+    A0_fig.tight_layout()
+
+    A1_fig_2, A1_ax_2 = plt.subplots()
+    A1_ax_2.plot(r_small, np.sqrt(-A1_q1_small))
+    A1_ax_2.set_xlabel(r'$r$')
+    A1_ax_2.set_ylabel(r'$\sqrt{A_{q_1}^{(1)}}$')
+    A1_ax_2.set_title(r'$\sqrt{A_{q_1}^{(1)}}$ for small $r$')
+    A1_fig_2.tight_layout()
+
+    A1_fig_3, A1_ax_3 = plt.subplots()
+    A1_ax_3.plot(r_small, (-A1_q1_small)**(1 / 3))
+    A1_ax_3.set_xlabel(r'$r$')
+    A1_ax_3.set_ylabel(r'$(A_{q_1}^{(1)})^{1/3}$')
+    A1_ax_3.set_title(r'$(A_{q_1}^{(1)})^{1/3}$ for small $r$')
+    A1_fig_3.tight_layout()
+
+    plt.show()
+
+    # # show log of each to check power
+    # An_q1[:, 0] -= np.min(An_q1[:, 0]) - 1e-8
+    # An_q1[:, 1] -= np.max(An_q1[:, 1]) + 1e-8
+    # An_q1[:, 0] = np.log(An_q1[:, 0])
+    # An_q1[:, 1] = np.log(-An_q1[:, 1])
+
+    # (fig_An_q1, 
+    #  ax1_An_q1, 
+    #  ax2_An_q1) = plot_two_fourier_coeffs(An_q1, 
+    #                                       R[0, :],
+    #                                       ylabel1=r'$q_1^{(0)} (r)$',
+    #                                       ylabel2=r'$-q_1^{(1)} (r)$')
+    # plt.show()
 
 
 
