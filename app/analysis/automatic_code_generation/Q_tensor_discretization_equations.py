9821da1230a88220a411c53faf5d0ea1c519f59c
Lucas Myers <lucasmyers97@gmail.com>
2023-09-01 16:17:16 -0500

    Wrote out discretization equations for Q-tensor

---
+++ app/analysis/automatic_code_generation/Q_tensor_discretization_equations.py
@@ -0,0 +1,232 @@
+"""
+Holds functions which symbolically produce the residual and jacobian terms for 
+different discretization techniques.
+"""
+
+import sympy as sy
+from ..utilities import tensor_calculus as tc
+
+def E1(Phi_i, Q, xi):
+    vec_dim = len(Phi_i)
+    E1 = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        E1[i] = -tc.grad(Phi_i[i], xi).ip( tc.grad(Q, xi) )
+
+    return sy.simplify(E1)
+
+
+
+def E2(Phi_i, Q, xi):
+    vec_dim = len(Phi_i)
+    E2 = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        E2[i] = -tc.transpose_3( tc.grad(Phi_i[i], xi) ).ip( tc.grad(Q, xi) )
+
+    return sy.simplify(E2)
+
+
+
+def E31(Phi_i, Q, xi):
+    vec_dim = len(Phi_i)
+    E31 = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        E31[i] = -tc.grad(Phi_i[i], xi).ip( Q * tc.grad(Q, xi) )
+
+    return sy.simplify(E31)
+
+
+
+def E32(Phi_i, Q, xi):
+    vec_dim = len(Phi_i)
+    E32 = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        E32[i] = -sy.Rational(1, 2) * Phi_i[i].ip( tc.grad(Q, xi) ** tc.transpose_3(tc.grad(Q, xi)) )
+
+    return sy.simplify(E32)
+
+
+
+def dE1(Phi_i, Phi_j, xi):
+    vec_dim = len(Phi_i)
+    dE1 = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dE1[i, j] = -tc.grad(Phi_i[i], xi).ip( tc.grad(Phi_j[j], xi) )
+
+    return sy.simplify(dE1)
+
+
+
+def dE2(Phi_i, Phi_j, xi):
+    vec_dim = len(Phi_i)
+    dE2 = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dE2[i, j] = -tc.grad(Phi_i[i], xi).ip( tc.transpose_3(tc.grad(Phi_j[j], xi)) )
+
+    return sy.simplify(dE2)
+
+
+
+def dE31(Phi_i, Phi_j, Q, xi):
+    vec_dim = len(Phi_i)
+    dE31 = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dE31[i, j] = -tc.grad(Phi_i[i]).ip( Phi_j[j] * tc.grad(Q, xi) + Q * tc.grad(Phi_j[j]) )
+
+    return sy.simplify(dE31)
+
+
+
+def dE32(Phi_i, Phi_j, Q, xi):
+    vec_dim = len(Phi_i)
+    dE32 = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dE32[i, j] = -Phi_i[i].ip( tc.grad(Phi_j[j], xi) ** tc.transpose_3(tc.grad(Q, xi)) )
+
+    return sy.simplify(dE32)
+
+
+
+
+def calc_singular_potential_convex_splitting_residual(Phi_i, Q, Q0, Lambda, xi, alpha, dt, L2, L3):
+
+    vec_dim = len(Phi_i)
+    RQ = sy.zeros(vec_dim)
+    RLambda = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        RQ[i] = Phi_i[i].ip(Q - (1 + alpha * dt) * Q0)
+        RLambda[i] = -dt * (-Phi_i[i].ip(Lambda))
+
+    RQ = sy.simplify(RQ)
+    RLambda = sy.simplify(RLambda)
+
+    RE1 = -dt * E1(Phi_i, Q, xi)
+    RE2 = -dt * L2 * E2(Phi_i, Q, xi)
+    RE31 = -dt * L3 * E31(Phi_i, Q, xi)
+    RE32 = -dt * L3 * E32(Phi_i, Q, xi)
+
+    return RQ, RLambda, RE1, RE2, RE31, RE32
+
+
+
+def calc_singular_potential_convex_splitting_jacobian(Phi_i, Phi_j, Q, dLambda, xi, dt, L2, L3):
+
+    vec_dim = len(Phi_i)
+    dRQ = sy.zeros(vec_dim, vec_dim)
+    dRLambda = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dRQ[i, j] = Phi_i[i].ip(Phi_j[j])
+            dRLambda[i, j] = -dt * (-Phi_i[i].ip(dLambda[j]))
+
+    dRQ = sy.simplify(dRQ) 
+    dRLambda = sy.simplify(dRLambda) 
+
+    dRE1 = -dt * dE1(Phi_i, Phi_j, xi)
+    dRE2 = -dt * L2 * dE2(Phi_i, Phi_j, xi)
+    dRE31 = -dt * L3 * dE31(Phi_i, Phi_j, Q, xi)
+    dRE32 = -dt * L3 * dE32(Phi_i, Phi_j, Q, xi)
+
+    return dRQ, dRLambda, dRE1, dRE2, dRE31, dRE32
+
+
+
+def calc_singular_potential_semi_implicit_residual(Phi_i, xi, Q, Q0, Lambda, Lambda0, dt, alpha, L2, L3, theta):
+
+    vec_dim = len(Phi_i)
+    RQ = sy.zeros(vec_dim)
+    RLambda = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        RQ[i] = Phi_i[i].ip( Q - Q0 - dt * (theta * alpha * Q0 + (1 - theta) * alpha * Q) )
+        RLambda[i] = -dt * Phi_i[i].ip( theta * (-Lambda0) + (1 - theta) * (-Lambda) )
+
+    RQ = sy.simplify(RQ)
+    RLambda = sy.simplify(RLambda)
+
+    RE1 = -dt * (theta * E1(Phi_i, Q0, xi) + (1 - theta) * E1(Phi_i, Q, xi))
+    RE2 = -dt * (theta * L2 * E2(Phi_i, Q0, xi) + (1 - theta) * L2 * E2(Phi_i, Q, xi))
+    RE31 = -dt * (theta * L3 * E31(Phi_i, Q0, xi) + (1 - theta) * L3 * E31(Phi_i, Q, xi))
+    RE32 = -dt * (theta * L3 * E32(Phi_i, Q0, xi) + (1 - theta) * L3 * E32(Phi_i, Q, xi))
+
+    return RQ, RLambda, RE1, RE2, RE31, RE32
+
+
+
+def calc_singular_potential_semi_implicit_jacobian(Phi_i, Phi_j, xi, Q, dLambda, dt, alpha, L2, L3, theta):
+
+    vec_dim = len(Phi_i)
+    dRQ = sy.zeros(vec_dim, vec_dim)
+    dRLambda = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dRQ[i, j] = Phi_i[i].ip(Phi_j[j]) - dt * (1 - theta) * alpha * Phi_i[i].ip(Phi_j[j])
+            dRLambda[i, j] = -dt * (1 - theta) * (-Phi_i[i].ip(dLambda[j]))
+
+    dRQ = sy.simplify(dRQ) 
+    dRLambda = sy.simplify(dRLambda) 
+
+    dRE1 = -dt * (1 - theta) * dE1(Phi_i, Phi_j, xi)
+    dRE2 = -dt * (1 - theta) * L2 * dE2(Phi_i, Phi_j, xi)
+    dRE31 = -dt * (1 - theta) * L3 * dE31(Phi_i, Phi_j, Q, xi)
+    dRE32 = -dt * (1 - theta) * L3 * dE32(Phi_i, Phi_j, Q, xi)
+
+    return dRQ, dRLambda, dRE1, dRE2, dRE31, dRE32
+
+
+def calc_newton_method_residual(Phi_i, xi, Q, Lambda, alpha, L2, L3):
+
+    vec_dim = len(Phi_i)
+    RQ = sy.zeros(vec_dim)
+    RLambda = sy.zeros(vec_dim)
+
+    for i in range(vec_dim):
+        RQ[i] = Phi_i[i].ip(alpha * Q)
+        RLambda[i] = Phi_i[i].ip(-Lambda)
+
+    RQ = sy.simplify(RQ)
+    RLambda = sy.simplify(RLambda)
+
+    RE1 = E1(Phi_i, Q, xi)
+    RE2 = L2 * E2(Phi_i, Q, xi)
+    RE31 = L3 * E31(Phi_i, Q, xi)
+    RE32 = L3 * E32(Phi_i, Q, xi)
+
+    return RQ, RLambda, RE1, RE2, RE31, RE32
+
+
+
+def calc_newton_method_jacobian(Phi_i, Phi_j, xi, Q, dLambda, alpha, L2, L3):
+
+    vec_dim = len(Phi_i)
+    dRQ = sy.zeros(vec_dim, vec_dim)
+    dRLambda = sy.zeros(vec_dim, vec_dim)
+
+    for i in range(vec_dim):
+        for j in range(vec_dim):
+            dRQ[i, j] = Phi_i[i].ip(alpha * Phi_j[j])
+            dRLambda[i, j] = Phi_i[i].ip(-dLambda[j])
+
+    dRQ = sy.simplify(dRQ) 
+    dRLambda = sy.simplify(dRLambda) 
+
+    dRE1 = dE1(Phi_i, Phi_j, xi)
+    dRE2 = L2 * dE2(Phi_i, Phi_j, xi)
+    dRE31 = L3 * dE31(Phi_i, Phi_j, Q, xi)
+    dRE32 = L3 * dE32(Phi_i, Phi_j, Q, xi)
+
+    return dRQ, dRLambda, dRE1, dRE2, dRE31, dRE32
--- app/analysis/automatic_code_generation/generate_Q_tensor_assembly.py
+++ app/analysis/automatic_code_generation/generate_Q_tensor_assembly.py
@@ -5,10 +5,18 @@ import sympy as sy
 
 from ..utilities import tensor_calculus as tc
 from ..utilities import dealii_code_generation as dcg
+from . import Q_tensor_discretization_equations as qtde
 
 class Basis(enum.Enum):
     component_wise = enum.auto()
     orthogonal = enum.auto()
+
+
+
+class Discretization(enum.Enum):
+    convex_splitting = enum.auto()
+    semi_implicit = enum.auto()
+    newton_method = enum.auto()
 
 
 
@@ -90,65 +98,8 @@ def get_3x3_traceless_symmetric_tensor_basis(basis_enum):
 
 
 
-def calc_singular_potential_convex_splitting_residual(Phi_i, Q, Q0, Lambda, xi, alpha, dt, L2, L3):
-
-    vec_dim = len(Phi_i)
-    R1 = sy.zeros(vec_dim, 1)
-    R2 = sy.zeros(vec_dim, 1)
-    R3 = sy.zeros(vec_dim, 1)
-    E1 = sy.zeros(vec_dim, 1)
-    E2 = sy.zeros(vec_dim, 1)
-    E31 = sy.zeros(vec_dim, 1)
-    E32 = sy.zeros(vec_dim, 1)
-
-    for i in range(vec_dim):
-        R1[i, 0] = Phi_i[i].ip(Q)
-        R2[i, 0] = -(1 + alpha * dt) * Phi_i[i].ip(Q0)
-        R3[i, 0] = -dt * (-Phi_i[i].ip(Lambda))
-        E1[i, 0] = -tc.grad(Phi_i[i], xi).ip(tc.grad(Q, xi))
-        E2[i, 0] = -tc.transpose_3( tc.grad(Phi_i[i], xi) ).ip(tc.grad(Q, xi))
-        E31[i, 0] = -tc.grad(Phi_i[i], xi).ip(Q * tc.grad(Q, xi))
-        E32[i, 0] = -sy.Rational(1, 2) * Phi_i[i].ip(tc.grad(Q, xi) ** tc.transpose_3( tc.grad(Q, xi) ))
-
-    R1 = sy.simplify(R1)
-    R2 = sy.simplify(R2)
-    R3 = sy.simplify(R3)
-    E1 = sy.simplify(E1)
-    E2 = sy.simplify(E2)
-    E31 = sy.simplify(E31)
-    E32 = sy.simplify(E32)
-
-    return R1, R2, R3, -dt*E1, -dt*L2*E2, -dt*L3*(E31 + E32)
-
-
-
-def calc_singular_potential_convex_splitting_jacobian(Phi_i, Phi_j, Q, dLambda, xi, alpha, dt, L2, L3):
-
-    vec_dim = len(Phi_i)
-    dR1 = sy.zeros(vec_dim, vec_dim)
-    dR2 = sy.zeros(vec_dim, vec_dim)
-    dE1 = sy.zeros(vec_dim, vec_dim)
-    dE2 = sy.zeros(vec_dim, vec_dim)
-    dE31 = sy.zeros(vec_dim, vec_dim)
-    dE32 = sy.zeros(vec_dim, vec_dim)
-
-    for i in range(vec_dim):
-        for j in range(vec_dim):
-            dR1[i, j] = Phi_i[i].ip(Phi_j[j])
-            dR2[i, j] = dt * Phi_i[i].ip(dLambda[j])
-            dE1[i, j] = -tc.grad(Phi_i[i], xi).ip(tc.grad(Phi_j[j], xi))
-            dE2[i, j] = -tc.transpose_3( tc.grad(Phi_i[i], xi) ).ip( tc.grad(Phi_j[j], xi) )
-            dE31[i, j] = -tc.grad(Phi_i[i], xi).ip(Phi_j[j]*tc.grad(Q, xi) + Q*tc.grad(Phi_j[j], xi))
-            dE32[i, j] = -Phi_i[i].ip(tc.grad(Phi_j[j], xi) ** tc.transpose_3( tc.grad(Q, xi) ))
-
-    dR1 = sy.simplify(dR1) 
-    dR2 = sy.simplify(dR2) 
-    dE1 = sy.simplify(dE1) 
-    dE2 = sy.simplify(dE2) 
-    dE31 = sy.simplify(dE31)
-    dE32 = sy.simplify(dE32)
-
-    return dR1, dR2, -dt*dE1, -dt*L2*dE2, -dt*L3*(dE31 + dE32)
+def get_discretization_expressions():
+    print('hello')
 
 
 
@@ -185,63 +136,48 @@ def main():
     Phi_i = tc.make_basis_functions(phi_i, basis)
     Phi_j = tc.make_basis_functions(phi_j, basis)
 
-    legacy_code = tc.TensorCalculusArray.zeros(vec_dim, 1)
-    new_code = tc.TensorCalculusArray.zeros(vec_dim, 1)
-    index_code = tc.TensorCalculusArray.zeros(vec_dim, 1)
-    for m in range(vec_dim):
-        legacy_code[m] = Phi_i[m].ip(tc.grad(Q, xi) ** tc.grad(Q, xi))
-        new_code[m] = Phi_i[m].ip(tc.grad(Q, xi) ** tc.transpose_3(tc.grad(Q, xi))) 
-        index_code[m] = sum(Phi_i[m][i, j] * Q[k, l].diff(xi[i]) * Q[k, l].diff(xi[j])
-                            for i in range(3)
-                            for j in range(3)
-                            for k in range(3)
-                            for l in range(3))
+    dLambda_label = r'\frac{{\partial\ \Lambda_{} }}{{\partial\ Q_{} }}'
+    dLambda_mat = tc.make_function_matrix(vec_dim, dLambda_label, coords)
+    dLambda = tc.make_jacobian_matrix_list(dLambda_mat, Phi_j)
 
-    print(sy.simplify(legacy_code - index_code))
-    print()
-    print(sy.simplify(new_code - index_code))
+    alpha, dt, L2, L3 = sy.symbols(r'\alpha \delta\ t L_2 L_3')
+    residual_terms = qtde.calc_singular_potential_convex_splitting_residual(Phi_i, 
+                                                                            Q, 
+                                                                            Q0, 
+                                                                            Lambda, 
+                                                                            xi,
+                                                                            alpha, dt, L2, L3)
+    jacobian_terms = qtde.calc_singular_potential_convex_splitting_jacobian(Phi_i, 
+                                                                            Phi_j, 
+                                                                            Q, 
+                                                                            dLambda, 
+                                                                            xi,
+                                                                            alpha, dt, L2, L3)
+    symbols_code = {alpha: 'alpha', dt: 'dt', L2: 'L2', L3: 'L3'}
+    Q_code = {Q_vec[i]: 'Q_vec[q][{}]'.format(i) 
+              for i in range(Q_vec.shape[0])}
+    Q0_code = {Q0_vec[i]: 'Q0_vec[q][{}]'.format(i) 
+               for i in range(Q0_vec.shape[0])}
+    Lambda_code = {Lambda_vec[i]: 'Lambda_vec[q][{}]'.format(i) 
+                   for i in range(Lambda_vec.shape[0])}
+    Lambda0_code = {Lambda0_vec[i]: 'Lambda0_vec[q][{}]'.format(i) 
+                    for i in range(Lambda0_vec.shape[0])}
+    dQ_code = {Q_vec[i].diff(xi[j]): 'dQ[q][{}][{}]'.format(i, j)
+               for i in range(vec_dim)
+               for j in range(3)}
+    dLambda_code = {dLambda_mat[i, j]: 'dLambda_dQ[{}][{}]'.format(i, j)
+                    for i in range(dLambda_mat.shape[0])
+                    for j in range(dLambda_mat.shape[1])}
+    phi_i_code = {phi_i: 'fe_values.shape_value(i, q)'}
+    phi_j_code = {phi_j: 'fe_values.shape_value(j, q)'}
 
-    # dLambda_label = r'\frac{{\partial\ \Lambda_{} }}{{\partial\ Q_{} }}'
-    # dLambda_mat = tc.make_function_matrix(vec_dim, dLambda_label, coords)
-    # dLambda = tc.make_jacobian_matrix_list(dLambda_mat, Phi_j)
+    user_funcs = symbols_code | Q_code | Q0_code | Lambda_code | Lambda0_code | dQ_code | phi_i_code | phi_j_code | dLambda_code
 
-    # singular_potential_symbols = sy.symbols(r'\alpha \delta\ t L_2 L_3')
-    # # residual_terms = calc_singular_potential_convex_splitting_residual(Phi_i, 
-    # #                                                                    Q, 
-    # #                                                                    Q0, 
-    # #                                                                    Lambda, 
-    # #                                                                    xi,
-    # #                                                                    *singular_potential_symbols)
-    # # jacobian_terms = calc_singular_potential_convex_splitting_jacobian(Phi_i, 
-    # #                                                                    Phi_j, 
-    # #                                                                    Q, 
-    # #                                                                    dLambda, 
-    # #                                                                    xi,
-    # #                                                                    *singular_potential_symbols)
-    # symbols_code = ['alpha', 'dt', 'L2', 'L3']
-    # symbols_list = [list(singular_potential_symbols),
-    #                 symbols_code]
+    printer = dcg.MyPrinter(user_funcs)
+    for i in range(vec_dim):
+        print( printer.doprint(residual_terms[-1][i]) )
 
-    # Q_code = 'Q_vec[q][{}]'
-    # Q0_code = 'Q0_vec[q][{}]'
-    # Lambda_code = 'Lambda_vec[{}]'
-    # Lambda0_code = 'Lambda0_vec[{}]'
-    # dLambda_code = 'dLambda_dQ[{}][{}]'
-    # phi_i_code = 'fe_values.shape_value(i, q)'
-    # phi_j_code = 'fe_values.shape_value(j, q)'
-    # function_list = [(Q_vec.tolist(), Q_code),
-    #                  (Q0_vec.tolist(), Q0_code),
-    #                  (Lambda_vec.tolist(), Lambda_code),
-    #                  (Lambda0_vec.tolist(), Lambda0_code),
-    #                  (dLambda_mat.tolist(), dLambda_code),
-    #                  ([phi_i], phi_i_code),
-    #                  ([phi_j], phi_j_code)]
-
-    # print(dLambda_mat[0][1])
-
-    # # printer = dcg.MyPrinter(function_list)
-    # #     
-    # # print(printer.doprint(residual_terms[0][0]))
+    sy.printing.preview(residual_terms[-1])
 
     # # for term in residual_terms:
     # #     sy.pprint(term)
