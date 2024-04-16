0d8d70c191b8d63ab2e194b53f05806db3d7d82b
Lucas Myers <lucasmyers97@gmail.com>
2024-04-16 10:37:33 -0500

    Added scripts to plot ER and TER data

---
+++ app/analysis/plotting/plot_alpha_angle_vs_L2.py
@@ -0,0 +1,68 @@
+
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['figure.dpi'] = 300
+
+plt.style.use('science')
+
+def get_commandline_args():
+
+
+    description = ('Plot alpha angle vs. L2 for TER configurations')
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--data_folder', 
+                        help='folder where alpha angle data lives')
+    parser.add_argument('--data_filename',
+                        help='excel file where alpha angle data lives')
+    parser.add_argument('--output_folder',
+                        default=None,
+                        help='folder that output file will be written to')
+    parser.add_argument('--plot_filename',
+                        help='filename of alpha vs. position plot')
+    parser.add_argument('--title',
+                        help='title of alpha vs. position plot')
+
+    args = parser.parse_args()
+
+    output_folder = None
+    if not args.output_folder:
+        output_folder = args.data_folder
+    else:
+        output_folder = args.output_folder
+
+    data_filename = os.path.join(args.data_folder, args.data_filename)
+    plot_filename = os.path.join(output_folder, args.plot_filename)
+
+    return data_filename, plot_filename, args.title
+
+
+
+def main():
+
+    data_filename, plot_filename, title = get_commandline_args()
+
+    data = pd.read_excel(data_filename)
+    TER_data = data[data['TER'] == 1]
+
+    fig, ax = plt.subplots()
+    ax.plot(TER_data['L2'], TER_data['alpha'], ls='', marker='o')
+    ax.set_title(title)
+    ax.set_xlabel(r'$L_2$')
+    ax.set_ylabel(r'$\alpha(r = 0)$')
+    fig.tight_layout()
+    fig.savefig(plot_filename)
+
+    plt.show()
+
+
+
+if __name__ == '__main__':
+
+    main()
+
