12acd752ecf105d926e2afb4c3ece4267f230410
Lucas Myers <lucasmyers97@gmail.com>
2025-04-16 12:58:00 -0500

    Updated scripts to finish paper revisions

--- app/analysis/LdG_to_frank.py
+++ app/analysis/LdG_to_frank.py
@@ -9,8 +9,8 @@ K1_expr = 4 * S**2 + 2 * L2 * S**2 - Rational(4, 3) * L3 * S**2
 K2_expr = 4 * S**2 - Rational(4, 3) * L3 * S**3
 K3_expr = 4 * S**2 + 2 * L2 * S**2 + Rational(8, 3) * L3 * S**3
 
-preview( simplify(K2_expr/K1_expr), output='svg' )
-preview( simplify(K3_expr/K1_expr), output='svg' )
+# preview( simplify(K2_expr/K1_expr), output='svg' )
+# preview( simplify(K3_expr/K1_expr), output='svg' )
 
 # 
 # print('L2 = {}'.format(L2_val))
@@ -29,18 +29,21 @@ preview( simplify(K3_expr/K1_expr), output='svg' )
 # Solve for L2 and L3
 solution = solve([K2_expr/K1_expr - K2, K3_expr/K1_expr - K3], [L2, L3], dict=True)[0]
 
+# preview(simplify(solution[L2]), output='svg')
+preview(simplify(solution[L3]), output='svg')
+
 # preview( simplify(solution[L2]), output='svg' )
 # preview( solution[L3], output='svg' )
 
-K2_val = 1/6
-print(K2_val)
-for K3_val in (0.8, 0.9, 1, 1.25, 1.5, 2, 2.5):
-    subs_dict = {S: 0.6751, K2: K2_val, K3: K3_val}
-    
-    print(subs_dict)
-    print( 'L2: {}'.format(solution[L2].subs(subs_dict)), end=' ' )
-    print( 'L3: {}'.format(solution[L3].subs(subs_dict)), end='\n\n' )
-
-
-subs_dict = {S: 0.6751, L2: 6.25, L3: 0.0}
+# K2_val = 1/6
+# print(K2_val)
+# for K3_val in (0.8, 0.9, 1, 1.25, 1.5, 2, 2.5):
+#     subs_dict = {S: 0.6751, K2: K2_val, K3: K3_val}
+#     
+#     print(subs_dict)
+#     print( 'L2: {}'.format(solution[L2].subs(subs_dict)), end=' ' )
+#     print( 'L3: {}'.format(solution[L3].subs(subs_dict)), end='\n\n' )
+# 
+# 
+subs_dict = {S: 0.6751, L2: 1.5, L3: 0.0}
 print( 'K2/K1 is given by: {}'.format(simplify(K2_expr/K1_expr).subs(subs_dict)), end=' ' )
